e6d9de37c4ceb05b3a450eeea0788e6e7bb73ae9
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2020-03-10 16:46:35 +1100

    Modify User model

--- Sources/AppStoreConnectCLI/Model/User.swift
+++ Sources/AppStoreConnectCLI/Model/User.swift
@@ -3,12 +3,12 @@
 import Foundation
 import AppStoreConnect_Swift_SDK
 
-struct User: Decodable {
+struct User: Codable {
     var username: String
     var firstName: String
     var lastName: String
-    var roles: UserRole
+    var roles: [UserRole]
     var provisioningAllowed: Bool
     var allAppsVisible: Bool
-    var visibleApps: [String]
+    var visibleApps: [String]?
 }
