6e3e7f197810a19bc01d5aeb8aa2e53aec66b96d
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2020-03-11 16:51:23 +1100

    Add User support for text presentation

---
+++ Sources/AppStoreConnectCLI/Model/API/ListUsers.Sort+init.swift
@@ -0,0 +1,8 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import ArgumentParser
+import AppStoreConnect_Swift_SDK
+import Foundation
+
+extension ListUsers.Sort: Codable, ExpressibleByArgument {
+}
--- Sources/AppStoreConnectCLI/Model/User.swift
+++ Sources/AppStoreConnectCLI/Model/User.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import AppStoreConnect_Swift_SDK
+import SwiftyTextTable
 
 struct User: Codable {
     var username: String
@@ -11,4 +12,57 @@ struct User: Codable {
     var provisioningAllowed: Bool
     var allAppsVisible: Bool
     var visibleApps: [String]?
+}
+
+// TODO: Extract these extensions somewhere that makes sense down the road
+
+// MARK: - API conveniences
+
+extension User {
+    static func fromAPIUser(_ apiUser: AppStoreConnect_Swift_SDK.User) -> User? {
+        guard let attributes = apiUser.attributes,
+              let username = attributes.username else {
+            // TODO: Error handling
+            return nil
+        }
+        let visibleApps = apiUser.relationships?.visibleApps?.data?.map { $0.type }
+        return User(username: username,
+                    firstName: attributes.firstName ?? "",
+                    lastName: attributes.lastName ?? "",
+                    roles: attributes.roles ?? [],
+                    provisioningAllowed: attributes.provisioningAllowed ?? false,
+                    allAppsVisible: attributes.allAppsVisible ?? false,
+                    visibleApps: visibleApps)
+    }
+}
+
+// MARK: - TextTable conveniences
+
+extension User {
+    static func getTableColumns(includeVisibleApps: Bool) -> [TextTableColumn] {
+        var columns = [
+            TextTableColumn(header: "Username"),
+            TextTableColumn(header: "First Name"),
+            TextTableColumn(header: "Last Name"),
+            TextTableColumn(header: "Role"),
+            TextTableColumn(header: "Provisioning Allowed"),
+            TextTableColumn(header: "All Apps Visible")
+        ]
+
+        if includeVisibleApps {
+            columns.append(TextTableColumn(header: "Visible Apps"))
+        }
+        return columns
+    }
+    func toTableRow() -> [CustomStringConvertible] {
+        return [
+            username,
+            firstName,
+            lastName,
+            roles.map { $0.rawValue }.joined(separator: ", "),
+            provisioningAllowed.toYesNo(),
+            allAppsVisible.toYesNo(),
+            visibleApps?.joined(separator: ", ") ?? ""
+        ]
+    }
 }
