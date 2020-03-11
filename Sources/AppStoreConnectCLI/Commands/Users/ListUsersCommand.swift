de2f112d14f5a79333c3d501ede4499ec6ff0618
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2020-03-11 16:58:05 +1100

    Complete User listing command

--- Sources/AppStoreConnectCLI/Commands/Users/ListUsersCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Users/ListUsersCommand.swift
@@ -2,6 +2,7 @@
 
 import AppStoreConnect_Swift_SDK
 import ArgumentParser
+import Combine
 import Foundation
 import SwiftyTextTable
 import Yams
@@ -13,20 +14,23 @@ public struct ListUsersCommand: ParsableCommand {
 
     public init() {}
 
+    @Option(default: "../config/auth.yml", help: "The APIConfiguration.")
+    var auth: String
+
     @Option(help: "Limit the number of users to return (maximum 200).")
     var limit: Int?
 
     @Option(
         parsing: SingleValueParsingStrategy.unconditional,
-        help: "Sort the results using the provided key (\(UserSorting.allCases.map { $0.rawValue }.joined(separator: ", "))).\nThe `-` prefix indicates descending order."
+        help: "Sort the results using the provided key (\(ListUsers.Sort.allCases.map { $0.rawValue }.joined(separator: ", "))).\nThe `-` prefix indicates descending order."
     )
-    var sort: UserSorting?
+    var sort: ListUsers.Sort?
 
     @Option(
         help: "Filter the results by the specified username.",
         transform: { $0.lowercased() }
     )
-    var filterUsername: String?
+    var filterUsername: [String?]
 
     @Option(
         parsing: ArrayParsingStrategy.singleValue,
@@ -48,113 +52,80 @@ public struct ListUsersCommand: ParsableCommand {
     var outputFormat: OutputFormat?
 
     public func run() throws {
-        var users = getMockUsers()
+        let authYml = try String(contentsOfFile: auth)
+        let configuration: APIConfiguration = try YAMLDecoder().decode(from: authYml)
+        let api = HTTPClient(configuration: configuration)
 
-        if let filterUsername = filterUsername {
-            users = users
-                .filter { $0.username.lowercased().contains(filterUsername) }
-        }
+        var filters = [ListUsers.Filter]()
 
         if !filterRole.isEmpty {
-            let roles = Set(filterRole)
-            users = users
-                .filter { !roles.intersection($0.roles).isEmpty }
+            filters.append(ListUsers.Filter.roles(filterRole.compactMap { $0?.rawValue }))
+        }
+
+        if !filterUsername.isEmpty {
+            filters.append(ListUsers.Filter.username(filterUsername.compactMap { $0 }))
         }
 
         if !filterVisibleApps.isEmpty {
-            let apps = Set(filterVisibleApps.compactMap { $0 })
-            users = users
-                .filter { user in
-                    guard let userApps = user.visibleApps else {
-                        return user.allAppsVisible
-                    }
-                    return user.allAppsVisible
-                        || !apps.intersection(userApps).isEmpty
-            }
+            filters.append(ListUsers.Filter.visibleApps(filterVisibleApps.compactMap { $0 }))
         }
 
-        if let sort = sort {
-            users = users.sorted { a, b in
-                switch sort {
-                    case .lastName:
-                        return a.lastName < b.lastName
-                    case .lastNameDesc:
-                        return a.lastName > b.lastName
-                    case .username:
-                        return a.username < b.username
-                    case .usernameDesc:
-                        return a.username > b.username
+        let request = APIEndpoint.users(fields: nil,
+                                        include: includeVisibleApps
+                                            ? [ListUsers.Include.visibleApps]
+                                            : nil,
+                                        limit: nil, // Limit of visibleApps if included, not limit of users
+                                        sort: [sort].compactMap { $0 },
+                                        filter: filters,
+                                        next: nil)
+
+        let _ = api.request(request)
+            .map { $0.data.compactMap(User.fromAPIUser) }
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print(String(describing: error))
                 }
-            }
-        }
+            }, receiveValue: { [self] users in
+                self.output(users)
+            })
+    }
 
+    func output(_ users: [User]) {
         if let outputFormat = outputFormat {
             do {
                 switch outputFormat {
-                case .json:
-                    let jsonEncoder = JSONEncoder()
-                    if #available(OSX 10.13, *) {
+                    case .json:
+                        let jsonEncoder = JSONEncoder()
                         jsonEncoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-                    } else {
-                        jsonEncoder.outputFormatting = [.prettyPrinted]
-                    }
 
-                    let redactedUsers = includeVisibleApps
-                        ? users
-                        : users.map { user in
-                            var copy = user
-                            copy.visibleApps = nil
-                            return copy
+                        let redactedUsers = includeVisibleApps
+                            ? users
+                            : users.map { user in
+                                var copy = user
+                                copy.visibleApps = nil
+                                return copy
                         }
 
-                    let json = try jsonEncoder.encode(redactedUsers)
-                    print(String(data: json, encoding: .utf8)!)
-                case .yaml:
-                    let yamlEncoder = YAMLEncoder()
-                    let yaml = try yamlEncoder.encode(users)
-                    print("users:\n" + yaml)
+                        var dict = [String:[User]]()
+                        dict["users"] = redactedUsers
+                        let json = try jsonEncoder.encode(dict)
+                        print(String(data: json, encoding: .utf8)!)
+                    case .yaml:
+                        let yamlEncoder = YAMLEncoder()
+                        let yaml = try yamlEncoder.encode(users)
+                        print("users:\n" + yaml)
                 }
             } catch {
                 print(error)
             }
 
         } else {
-            var columns = [
-                TextTableColumn(header: "Username"),
-                TextTableColumn(header: "First Name"),
-                TextTableColumn(header: "Last Name"),
-                TextTableColumn(header: "Role"),
-                TextTableColumn(header: "Provisioning Allowed"),
-                TextTableColumn(header: "All Apps Visible")
-            ]
-
-            if includeVisibleApps {
-                columns.append(TextTableColumn(header: "Visible Apps"))
-            }
-
+            let columns = User.getTableColumns(includeVisibleApps: includeVisibleApps)
             var table = TextTable(columns: columns)
-
-            users.forEach { user in
-                table.addRow(values: [
-                    user.username,
-                    user.firstName,
-                    user.lastName,
-                    user.roles.map { $0.rawValue }.joined(separator: ", "),
-                    user.provisioningAllowed ? "YES" : "NO",
-                    user.allAppsVisible ? "YES" : "NO",
-                    user.visibleApps?.joined(separator: ", ") ?? ""
-                ])
-            }
-
+            table.addRows(values: users.map { $0.toTableRow() })
             let str = table.render()
 
             print(str)
         }
     }
-}
-
-private func getMockUsers() -> [User] {
-    let one = User(username: "chris.kolbu", firstName: "Chris", lastName: "Kolbu", roles: [.appManager, .sales], provisioningAllowed: true, allAppsVisible: false, visibleApps: ["com.ittybittyapps.app1"])
-    let two = User(username: "other.user", firstName: "Awkward", lastName: "Aardvark", roles: [.accountHolder, .developer], provisioningAllowed: true, allAppsVisible: true, visibleApps: ["*"])
-    return [one, two]
 }
