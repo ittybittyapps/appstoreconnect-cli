eae16454c58ed1f607e9cb58605ffa375ec1d31f
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2020-03-11 16:57:16 +1100

    Add HTTPClient leveraging Combine and Laziness
    Due to the nature of the app as a CLI tool, it is necessary for the API calls to be blocking.
    
    For composability and general ease of reasoning it is also useful to be able to treat them reactively, using Combine.
    
    This also allows us to leverage Combine’s Deferred<>, allowing the HTTPClient to defer the actual API call until the Future is materialised/subscribed to.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "appstoreconnect-cli",
     platforms: [
-        .macOS(.v10_12)
+        .macOS(.v10_15)
     ],
     dependencies: [
         .package(
