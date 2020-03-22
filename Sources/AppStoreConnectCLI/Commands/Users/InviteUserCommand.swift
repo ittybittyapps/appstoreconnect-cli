774c2acab295132023a4bec5e041b89d7f6fdafd
Decheng <chinadecheng@gmail.com>
2020-03-22 15:43:31 +1100

    Implement Invite user command

--- Sources/AppStoreConnectCLI/Commands/Users/InviteUserCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Users/InviteUserCommand.swift
@@ -4,11 +4,19 @@ import AppStoreConnect_Swift_SDK
 import ArgumentParser
 import Foundation
 
-struct InviteUserCommand: ParsableCommand {
+//extension Array: ExpressibleByArgument where Element == String {
+//    public init?(argument: String) {
+//        self = [argument]
+//    }
+//}
 
+struct InviteUserCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "invite",
         abstract: "Invite a user with assigned user roles to join your team.")
+
+    @Option(default: "../config/auth.yml", help: "The APIConfiguration.")
+    var auth: String
 
     @Argument(help: "The email address of a pending user invitation. The email address must be valid to activate the account. It can be any email address, not necessarily one associated with an Apple ID.")
     var email: String
@@ -28,11 +36,36 @@ struct InviteUserCommand: ParsableCommand {
     @Flag(help: "Indicates the user's specified role allows access to the provisioning functionality on the Apple Developer website.")
     var provisioningAllowed: Bool
 
+    //TODO: parse two arrays as argument
+//    @Argument(help: "Array of opaque resource ID that uniquely identifies the resources.")
+//    var appsVisibleIds: [String]
+
     public func run() throws {
-        // TODO
-        print(self.email)
-        print(self.firstName)
-        print(self.lastName)
-        print(self.roles)
+        let api = try HTTPClient(auth: auth)
+
+        let request = APIEndpoint.invite(userWithEmail: email,
+                                         firstName: firstName,
+                                         lastName: lastName,
+                                         roles: roles,
+                                         allAppsVisible: allAppsVisible,
+                                         provisioningAllowed: provisioningAllowed,
+                                         appsVisibleIds: nil)
+
+        _ = api.request(request)
+            .map { $0.data }
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print(String(describing: error))
+                }
+            }, receiveValue: { (result: UserInvitation) -> Void in
+                print("Invitation email has been sent, invitation info: ")
+
+                let jsonEncoder = JSONEncoder()
+                jsonEncoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+
+                let data = try! jsonEncoder.encode(result)
+                print(String(data: data, encoding: .utf8)!)
+            }
+        )
     }
 }
