2428baf65b02f96a2c2e81fcd6b4732d74cdf403
Huw Rowlands <huw@ittybittyapps.com>
2020-03-31 16:07:07 +1100

    Simplify bundle identifier naming
    Previously, I had been inconsistent with naming between bundleId, the identifier for the bundle ID, and the App Store Connect internal resource identifier. This makes clear the difference between much of it.

--- Sources/AppStoreConnectCLI/Model/BundleId.swift
+++ Sources/AppStoreConnectCLI/Model/BundleId.swift
@@ -5,34 +5,7 @@ import Combine
 import Foundation
 import SwiftyTextTable
 
-enum BundleIdCapability: String, Decodable {
-    case icloud
-    case inAppPurchase
-    case gameCenter
-    case pushNotifications
-    case wallet
-    case interAppAudio
-    case maps
-    case associatedDomains
-    case personalVpn
-    case appGroups
-    case healthkit
-    case homekit
-    case wirelessAccessoryConfiguration
-    case applePay
-    case dataProtection
-    case sirikit
-    case networkExtensions
-    case multipath
-    case hotSpot
-    case nfcTagReading
-    case classkit
-    case autofillCredentialProvider
-    case accessWifiInformation
-}
-
 struct BundleId: ResultRenderable {
-    var id: String?
     var identifier: String?
     var name: String?
     var platform: BundleIdPlatform?
@@ -45,7 +18,6 @@ extension BundleId {
     init(_ apiBundleId: AppStoreConnect_Swift_SDK.BundleId) {
         let attributes = apiBundleId.attributes
         self.init(
-            id: apiBundleId.id,
             identifier: attributes?.identifier,
             name: attributes?.name,
             platform: attributes?.platform,
@@ -56,7 +28,6 @@ extension BundleId {
     init(response: AppStoreConnect_Swift_SDK.BundleIdResponse) {
         let attributes = response.data.attributes
         self.init(
-            id: response.data.id,
             identifier: attributes?.identifier,
             name: attributes?.name,
             platform: attributes?.platform,
@@ -89,24 +60,23 @@ extension BundleId: TableInfoProvider {
 
 extension HTTPClient {
 
-    /// Find the opaque identifier for this bundle ID.
+    /// Find the opaque internal identifier for this bundle ID.
     ///
-    /// This is an App Store Connect internal identifier; not the bundle-id. Use this for reading, modifying and deleting bundle-id resources.
-    func findInternalIdentifier(for bundleId: String) throws -> AnyPublisher<String, Error> {
+    /// This is an App Store Connect internal identifier; not the reverse-DNS bundleId identifier. Use this for reading, modifying and deleting bundleId resources.
+    func findInternalIdentifier(for identifier: String) throws -> AnyPublisher<String, Error> {
         let request = APIEndpoint.listBundleIds(
             filter: [
-                BundleIds.Filter.identifier([bundleId])
+                BundleIds.Filter.identifier([identifier])
             ]
         )
 
         return self.request(request)
-            .map { $0.data.map(BundleId.init) }
-            .map { $0.filter { $0.identifier == bundleId } }
+            .map { $0.data.filter { $0.attributes?.identifier == identifier } }
             .compactMap { response -> String? in
                 if response.count == 1 {
                     return response.first?.id
                 }
-                fatalError("Bundle ID '\(bundleId)' not unique or not found")
+                fatalError("Bundle ID identifier '\(identifier)' not unique or not found")
             }
             .eraseToAnyPublisher()
     }
