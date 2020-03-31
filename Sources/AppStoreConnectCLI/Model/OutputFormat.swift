2933074c0fac11de7c44af8c44bbb2eec6b39296
Huw Rowlands <huw@ittybittyapps.com>
2020-04-01 08:52:50 +1100

    Make `OutputFormat` `CustomStringConvertible`
    This makes it look good when used for the `default` value of and `@Option` annotated property. Previously, when it something like `.csv` was used as the default, the help text would read: `(default: AppStoreConnectCLI.OutputFormat.csv)`; now it says simply: `(default: csv)`.

--- Sources/AppStoreConnectCLI/Model/OutputFormat.swift
+++ Sources/AppStoreConnectCLI/Model/OutputFormat.swift
@@ -10,6 +10,12 @@ enum OutputFormat: String, CaseIterable, Codable {
     case yaml
 }
 
+extension OutputFormat: CustomStringConvertible {
+    var description: String {
+        self.rawValue
+    }
+}
+
 extension OutputFormat: ExpressibleByArgument {
     init?(argument: String) {
         self.init(rawValue: argument.lowercased())
