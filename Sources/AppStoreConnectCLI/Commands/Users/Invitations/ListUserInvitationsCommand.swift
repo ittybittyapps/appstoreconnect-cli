372ba8dfcc4ecea41f0d3d439283d9660cf5fb03
Huw Rowlands <huw@ittybittyapps.com>
2020-04-02 09:57:49 +1100

    List user invitations implementation
    Previously, this was stubbed.

--- Sources/AppStoreConnectCLI/Commands/Users/Invitations/ListUserInvitationsCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Users/Invitations/ListUserInvitationsCommand.swift
@@ -6,26 +6,55 @@ import Foundation
 
 struct ListUserInvitationsCommand: ParsableCommand {
     public static var configuration = CommandConfiguration(
-        commandName: "list-invites",
-        abstract: "Get a list of pending invitations to join your team.")
+        commandName: "list-invitations",
+        abstract: "Get a list of pending invitations to join your team."
+    )
+
+    @OptionGroup()
+    var authOptions: AuthOptions
 
     @Option(help: "Limit the number of users to return (maximum 200).")
     var limit: Int?
 
-    @Option(help: "Sort the results in the specified order.")
-    var sort: String?
+    @Option(parsing: .upToNextOption, help: "Filter the results by the specified username")
+    var filterEmail: [String]
 
-    @Option(help: "Filter the results by the specified username")
-    var filterEmail: String?
-
-    @Option(help: "Filter the results by the specified roles")
-    var filterRole: UserRole?
+    @Option(parsing: .upToNextOption, help: "Filter the results by the specified roles")
+    var filterRole: [UserRole]
 
     @Flag(help: "Include visible apps in results.")
     var includeVisibleApps: Bool
 
+    @Option(default: .table, help: "Return exportable results in provided format (\(OutputFormat.allCases.map { $0.rawValue }.joined(separator: ", "))).")
+    var outputFormat: OutputFormat
+
+    private var filters: [ListInvitedUsers.Filter]? {
+        var filters = [ListInvitedUsers.Filter]()
+
+        if filterEmail.isEmpty == false {
+            filters += [ListInvitedUsers.Filter.email(filterEmail)]
+        }
+
+        if filterRole.isEmpty == false {
+            filters += [ListInvitedUsers.Filter.roles(filterRole.map { $0.rawValue })]
+        }
+
+        return filters
+    }
+
     public func run() throws {
-        // TODO
-        print(self.sort as Any)
+        let api = HTTPClient(configuration: APIConfiguration.load(from: authOptions))
+
+        let request = APIEndpoint.invitedUsers(
+            limit: limit.map { [ListInvitedUsers.Limit.visibleApps($0)] } ?? [],
+            filter: filters
+        )
+
+        _ = api.request(request)
+            .map { $0.data }
+            .sink(
+                receiveCompletion: Renderers.CompletionRenderer().render,
+                receiveValue: Renderers.ResultRenderer(format: outputFormat).render
+            )
     }
 }
--- Sources/AppStoreConnectCLI/Commands/Users/UsersCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Users/UsersCommand.swift
@@ -9,7 +9,7 @@ struct UsersCommand: ParsableCommand {
         abstract: "User commands",
         subcommands: [
             InviteUserCommand.self,
-            // TODO: GetUserInvitationInfoCommand.self, // Get information about a pending invitation to join your team.
+            ListUserInvitationsCommand.self,
             // TODO: CancelUserInvitationCommand.self, // Cancel a pending invitation for a user to join your team.
             // TODO: ListInvitedUserVisibleAppsCommand.self, // Get a list of apps that will be visible to a user with a pending invitation.
             ListUsersCommand.self,
