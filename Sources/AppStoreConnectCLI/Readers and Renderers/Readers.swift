a863f0509f88dd6596c7bef25673b6cdb3ef36b9
Huw Rowlands <huw@ittybittyapps.com>
2020-04-02 14:16:57 +1100

    Move reading logic in to `Readers`
    Provides a generic `FileReader` which can read any kind of decodable thing.
    
    (Except CSV…… which I haven't worked out yet)

---
+++ Sources/AppStoreConnectCLI/Readers and Renderers/Readers.swift
@@ -0,0 +1,58 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import Foundation
+import CodableCSV
+import Combine
+import SwiftyTextTable
+import Yams
+import AppStoreConnect_Swift_SDK
+
+protocol Reader {
+    associatedtype Output
+
+    func read(filePath: String) -> Output
+}
+
+enum Readers {
+
+    struct FileReader<T: Decodable>: Reader {
+        let format: InputFormat
+
+        func read(filePath: String) -> T {
+            switch format {
+            case .csv:
+                return readCSV(from: filePath)
+            case .json:
+                return readJSON(from: filePath)
+            case .yaml:
+                return readYAML(from: filePath)
+            }
+        }
+
+        private func readCSV<T: Decodable>(from filePath: String) -> T {
+            fatalError("Not implemented")
+        }
+
+        private func readJSON<T: Decodable>(from filePath: String) -> T {
+            guard
+                let fileContents = try? String(contentsOfFile: filePath, encoding: .utf8),
+                let data = fileContents.data(using: .utf8),
+                let result = try? JSONDecoder().decode(T.self, from: data) else {
+                    fatalError("Could not read JSON file: \(filePath)")
+            }
+
+            return result
+        }
+
+        private func readYAML<T: Decodable>(from filePath: String) -> T {
+            guard
+                let fileContents = try? String(contentsOfFile: filePath, encoding: .utf8),
+                let result = try? YAMLDecoder().decode(T.self, from: fileContents) else {
+                    fatalError("Could not read YAML file: \(filePath)")
+            }
+
+            return result
+        }
+    }
+
+}
