17fd5f237546ab844e742e5365cb465a0c42badf
Oliver Jones <oliver@deeperdesign.com>
2020-04-05 13:04:45 +1000

    Refactor AuthOptions to be leaner and meaner

--- Sources/AppStoreConnectCLI/Commands/CommonParsableCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/CommonParsableCommand.swift
@@ -12,25 +12,129 @@ protocol CommonParsableCommand: ParsableCommand {
 
 extension CommonParsableCommand {
     func makeClient() throws -> HTTPClient {
-        HTTPClient(configuration: APIConfiguration.load(from: common.authOptions))
+        HTTPClient(configuration: try APIConfiguration(common.authOptions))
     }
 }
 
 struct AuthOptions: ParsableArguments {
-    @Option(default: "config/auth.yml", help: "The APIConfiguration.")
-    var auth: String?
 
-    @Option(help: "Your issuer ID from the API Keys page in App Store Connect (Ex: 12345678-90ab-cdef-0987-654321abcdef)")
-    var issuerId: String?
+    enum Error: Swift.Error {
+        case issuerNotProvided
+        case apiKeyIdNotProvided
+    }
 
-    @Option(help: "Your private key ID from App Store Connect (Ex: ABCDEF0123)")
-    var privateKeyID: String?
+    @Option(
+        default: .environment("APPSTORE_CONNECT_ISSUER_ID"),
+        help: ArgumentHelp(
+            "An AppStore Connect API Key issuer ID.",
+            discussion:
+                """
+                This value can be obtained from the AppStore Connect portal and takes the form of a UUID.
 
-    @Option(help: "Your private key from App Store Connect")
-    var privateKey: String?
+                If not specified on the command line this value will be read from the environment variable APPSTORE_CONNECT_ISSUER_ID.
+                """,
+            valueName: "uuid"
+        )
+    )
+    var apiIssuer: IssuerID
 
-    @Option(help: "Your private API key file(.p8) path downloaded from App Store Connect")
-    var privateKeyFilePath: String?
+    @Option(
+        default: .environment("APPSTORE_CONNECT_API_KEY_ID"),
+        help: ArgumentHelp(
+            "An AppStoreConnect API Key ID.",
+            discussion:
+                """
+                This value can be obtained from the AppStore Connect portal and takes the form of an 10 alpha-numeric identifier, eg. 7MM5YSX5LS
+
+                This option will search the environment for a key with the name of \(APIKeyID.apiKeyEnvironmentKey). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
+
+                If environment variable is empty, in the incorrect format, or not found then this option will search following  directories in sequence for a private key file with the name of 'AuthKey_<api-key-id>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
+
+                If not specified on the command line the value of this option will be read from the environment variable APPSTORE_CONNECT_API_KEY_ID.
+                """
+        )
+    )
+    var apiKeyId: APIKeyID
+}
+
+protocol EnvironmentLoadableArgument: ExpressibleByArgument, CustomStringConvertible {
+    static var envPrefix: String { get }
+    var argument: String { get }
+    var value: String { get }
+}
+
+extension EnvironmentLoadableArgument {
+    static var envPrefix: String { "@env:" }
+
+    static func environment(_ variableName: String) -> Self {
+        Self(argument: "\(envPrefix)\(variableName)")!
+    }
+
+    var description: String { argument }
+
+    var value: String {
+       guard argument.hasPrefix(Self.envPrefix) else {
+           return argument
+       }
+
+       let envKey = String(argument.dropFirst(Self.envPrefix.count))
+       return ProcessInfo.processInfo.environment[envKey] ?? ""
+   }
+}
+
+struct IssuerID: EnvironmentLoadableArgument {
+    let argument: String
+
+    init?(argument: String) {
+        self.argument = argument
+    }
+}
+
+struct APIKeyID: EnvironmentLoadableArgument {
+
+    enum Error: Swift.Error {
+        case apiKeyNotFound
+    }
+
+    static let apiKeyEnvironmentKey = "APPSTORE_CONNECT_API_KEY"
+    static let searchPaths = ["./private_keys", "~/private_keys", "~/.private_keys", "~/.appstoreconnect/private_keys"]
+
+    var argument: String
+
+    init?(argument: String) {
+        self.argument = argument
+    }
+
+    private func findKeyFileURL() -> URL? {
+        let fileManager = FileManager()
+        let authKeyFilename = "AuthKey_\(value).p8"
+
+        return Self.searchPaths
+            .lazy
+            .map { $0.replacingOccurrences(of: "~", with: fileManager.homeDirectoryForCurrentUser.path) }
+            .map(URL.init(fileURLWithPath:))
+            .map { $0.appendingPathComponent(authKeyFilename) }
+            .first { fileManager.fileExists(atPath: $0.path) }
+    }
+
+    func load() throws -> String {
+
+        // TODO: validate the format of the env var content
+        // TODO: validate format of file is correct (if found)
+
+        guard let apiKeyFileContent =
+            try (ProcessInfo.processInfo.environment["APPSTORE_CONNECT_API_KEY"] ??
+                findKeyFileURL().map { try String(contentsOf: $0, encoding: .utf8) })
+        else {
+            throw Error.apiKeyNotFound
+        }
+
+
+        return apiKeyFileContent
+            .components(separatedBy: .newlines)
+            .filter { $0.hasSuffix("PRIVATE KEY-----") == false }
+            .joined()
+    }
 }
 
 struct CommonOptions: ParsableArguments {
--- Sources/AppStoreConnectCLI/Helpers/APIConfiguration+Helpers.swift
+++ Sources/AppStoreConnectCLI/Helpers/APIConfiguration+Helpers.swift
@@ -4,76 +4,23 @@ import Foundation
 import AppStoreConnect_Swift_SDK
 import Yams
 
+
+
 extension APIConfiguration {
-    static func load(from authOptions: AuthOptions) -> APIConfiguration {
-        // First use variables from command line
-        if let issuerId = authOptions.issuerId,
-            let privateKeyID = authOptions.privateKeyID {
+    init(_ authOptions: AuthOptions) throws {
 
-            if let privateKey = authOptions.privateKey {
-                return APIConfiguration(issuerId, privateKeyID, privateKey)
-            }
-
-            if let path = authOptions.privateKeyFilePath {
-                return APIConfiguration(issuerId, privateKeyID, readPrivateKeyFrom(filePath: path))
-            }
+        guard authOptions.apiIssuer.value.isEmpty == false else {
+            throw AuthOptions.Error.issuerNotProvided
         }
 
-        // Then use config file
-        if let authFilePath = authOptions.auth,
-            let authYml = try? String(contentsOfFile: authFilePath),
-            let configuration: APIConfiguration = try? YAMLDecoder().decode(from: authYml) {
-
-            return configuration
+        guard authOptions.apiKeyId.value.isEmpty == false else {
+            throw AuthOptions.Error.apiKeyIdNotProvided
         }
 
-        // Last, try to use environment variables
-        if let issuerId = ProcessInfo.appstoreIssuerId,
-            let privateKeyID = ProcessInfo.appstorePrivateKeyID {
-
-            if let privateKey = ProcessInfo.appstorePrivateKey {
-                return APIConfiguration(issuerId, privateKeyID, privateKey)
-            }
-
-            if let path = ProcessInfo.appstoreKeyFilePath {
-                return APIConfiguration(issuerId, privateKeyID, readPrivateKeyFrom(filePath: path))
-            }
-        }
-
-        fatalError("Can't load APIConfiguration, no auth config found")
-    }
-
-    static func readPrivateKeyFrom(filePath: String) -> String {
-        let apiKeyFileContent = try? String(contentsOfFile: filePath, encoding: .utf8)
-        let fileContentArray = apiKeyFileContent?.components(separatedBy: .newlines)
-
-        // Strip header and footer, then concatenate each line together
-        guard let privateKey = fileContentArray?.filter({ !$0.contains("-----") }).joined() else {
-            fatalError("Invalid private key file path provided, or file has invalid content")
-        }
-
-        return privateKey
-    }
-
-    init(_ issuerID: String, _ privateKeyID: String, _ privateKey: String) {
-        self.init(issuerID: issuerID, privateKeyID: privateKeyID, privateKey: privateKey)
-    }
-}
-
-private extension ProcessInfo {
-    static var appstoreIssuerId: String? {
-        self.processInfo.environment["APPSTORE_ISSUER_ID"]
-    }
-
-    static var appstorePrivateKeyID: String? {
-        self.processInfo.environment["APPSTORE_KEY_ID"]
-    }
-
-    static var appstorePrivateKey: String? {
-        self.processInfo.environment["APPSTORE_KEY"]
-    }
-
-    static var appstoreKeyFilePath: String? {
-        self.processInfo.environment["APPSTORE_KEY_FILE_PATH"]
-    }
+        self.init(
+            issuerID: authOptions.apiIssuer.value,
+            privateKeyID: authOptions.apiKeyId.value,
+            privateKey: try authOptions.apiKeyId.load()
+        )
+    }    
 }
