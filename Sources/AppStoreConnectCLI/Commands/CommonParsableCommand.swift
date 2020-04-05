407d29ddaf49f3402a93f1b504606fe0320f403f
Oliver Jones <oliver@deeperdesign.com>
2020-04-05 13:21:32 +1000

    Provide a shorter valueName

--- Sources/AppStoreConnectCLI/Commands/CommonParsableCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/CommonParsableCommand.swift
@@ -48,10 +48,11 @@ struct AuthOptions: ParsableArguments {
 
                 This option will search the environment for a key with the name of \(APIKeyID.apiKeyEnvironmentKey). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
 
-                If environment variable is empty, in the incorrect format, or not found then this option will search following  directories in sequence for a private key file with the name of 'AuthKey_<api-key-id>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
+                If environment variable is empty, in the incorrect format, or not found then this option will search following  directories in sequence for a private key file with the name of 'AuthKey_<keyid>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
 
                 If not specified on the command line the value of this option will be read from the environment variable APPSTORE_CONNECT_API_KEY_ID.
-                """
+                """,
+            valueName: "keyid"
         )
     )
     var apiKeyId: APIKeyID
