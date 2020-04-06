da6e7b07aa87bb71700c5327265faded36ccd862
Oliver Jones <oliver@deeperdesign.com>
2020-04-06 11:51:52 +1000

    Rework option documentation

--- Sources/AppStoreConnectCLI/Arguments/AuthOptions.swift
+++ Sources/AppStoreConnectCLI/Arguments/AuthOptions.swift
@@ -16,7 +16,7 @@ struct AuthOptions: ParsableArguments {
             "An AppStore Connect API Key issuer ID.",
             discussion:
                 """
-                This value can be obtained from the AppStore Connect portal and takes the form of a UUID.
+                The value for this option can be obtained from the AppStore Connect portal and takes the form of a UUID.
 
                 If not specified on the command line this value will be read from the environment variable APPSTORE_CONNECT_ISSUER_ID.
                 """,
@@ -31,13 +31,13 @@ struct AuthOptions: ParsableArguments {
             "An AppStoreConnect API Key ID.",
             discussion:
                 """
-                This value can be obtained from the AppStore Connect portal and takes the form of an 10 character alpha-numeric identifier, eg. 7MM5YSX5LS
-
-                This option will search the environment for a key with the name of \(APIKeyID.apiKeyEnvironmentKey). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
-
-                If environment variable is empty, in the incorrect format, or not found then this option will search following  directories in sequence for a private key file with the name of 'AuthKey_<keyid>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
+                The value for this option can be obtained from the AppStore Connect portal and takes the form of an 10 character alpha-numeric identifier, eg. 7MM5YSX5LS
 
                 If not specified on the command line the value of this option will be read from the environment variable APPSTORE_CONNECT_API_KEY_ID.
+
+                The environment will be searched for key with the name of \(APIKeyID.apiKeyEnvironmentKey). The contents of this environment key are expected to be a PKCS 8 (.p8) formatted private key associated with this Key ID.
+
+                If the \(APIKeyID.apiKeyEnvironmentKey) environment variable is empty, in the incorrect format, or not found then the following directories will be searched in sequence for a private key file with the name of 'AuthKey_<keyid>.p8': \(ListFormatter.localizedString(byJoining: APIKeyID.searchPaths)).
                 """,
             valueName: "keyid"
         )
