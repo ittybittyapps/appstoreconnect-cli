4da127137b4c243afed6ba376dd15c5f6b8ad4f2
Decheng <chinadecheng@gmail.com>
2020-04-15 16:43:36 +1000

    Rename betaTesterIdentifier -> betaTesterResourceId for consistency

--- Sources/AppStoreConnectCLI/Model/BetaTester.swift
+++ Sources/AppStoreConnectCLI/Model/BetaTester.swift
@@ -98,7 +98,7 @@ extension HTTPClient {
     /// Find the opaque internal identifier for this tester; search by email adddress.
     ///
     /// This is an App Store Connect internal identifier
-    func betaTesterIdentifier(matching email: String) throws -> AnyPublisher<String, Error> {
+    func betaTesterResourceId(matching email: String) throws -> AnyPublisher<String, Error> {
         let endpoint = APIEndpoint.betaTesters(
             filter: [ListBetaTesters.Filter.email([email])]
         )
@@ -114,117 +114,4 @@ extension HTTPClient {
             .eraseToAnyPublisher()
     }
 
-    private enum TransformStrategy {
-        case hasApps(appIds: [String])
-        case hasGroups(groupIds: [String])
-        case hasBoth(appIds: [String], groupIds: [String])
-        case none
-
-        init(groupIds: [String]?, appIds: [String]?) {
-            switch (groupIds, appIds) {
-                case let(.some(groupIds), _) where !groupIds.isEmpty:
-                    self = .hasGroups(groupIds:groupIds)
-                case let(_, .some(appIds)) where !appIds.isEmpty:
-                    self = .hasApps(appIds: appIds)
-                case let(.some(groupIds), .some(appIds)) where !groupIds.isEmpty && !appIds.isEmpty:
-                    self = .hasBoth(appIds: appIds, groupIds: groupIds)
-                case (_, _):
-                    self = .none
-            }
-        }
-    }
-
-    func fromAPIBetaTesters(betaTesters: [AppStoreConnect_Swift_SDK.BetaTester]) -> AnyPublisher<[BetaTester], Error> {
-        let betaTesters = betaTesters.map { (tester: AppStoreConnect_Swift_SDK.BetaTester) -> AnyPublisher<BetaTester, Error> in
-            let groupIds = tester.relationships?.betaGroups?.data?.compactMap { $0.id }
-            let appIds = tester.relationships?.apps?.data?.compactMap { $0.id }
-
-            switch TransformStrategy(groupIds: groupIds, appIds: appIds) {
-                case .hasApps(let appIds):
-                    let endpoint = APIEndpoint.apps(
-                        filters: [ListApps.Filter.id(appIds)]
-                    )
-
-                    return self.request(endpoint)
-                        .map(\.data)
-                        .map { (apps: [AppStoreConnect_Swift_SDK.App]) in
-                            let apps = apps.map { App(bundleId: $0.attributes?.bundleId,
-                                                      name: $0.attributes?.name,
-                                                      primaryLocale: $0.attributes?.primaryLocale,
-                                                      sku: $0.attributes?.sku) }
-
-                            return BetaTester(
-                                email: tester.attributes?.email,
-                                firstName: tester.attributes?.firstName,
-                                lastName: tester.attributes?.lastName,
-                                inviteType: tester.attributes?.inviteType,
-                                betaGroups: nil,
-                                apps: apps
-                            )
-                        }
-                        .eraseToAnyPublisher()
-
-                case .hasGroups(let groupIds):
-                    let endpoint = APIEndpoint.betaGroups(
-                        filter: [ListBetaGroups.Filter.id(groupIds)]
-                    )
-
-                    return self.request(endpoint)
-                        .map(\.data)
-                        .map { (groups: [BetaGroup]) in
-                            return BetaTester(
-                                email: tester.attributes?.email,
-                                firstName: tester.attributes?.firstName,
-                                lastName: tester.attributes?.lastName,
-                                inviteType: tester.attributes?.inviteType,
-                                betaGroups: groups,
-                                apps: nil)
-                        }
-                        .eraseToAnyPublisher()
-
-                case .hasBoth(let appIds, let groupIds):
-                    let betaGroupEndpoint = APIEndpoint.betaGroups(
-                        filter: [ListBetaGroups.Filter.id(groupIds)]
-                    )
-
-                    let appsEndpoint = APIEndpoint.apps(
-                        filters: [ListApps.Filter.id(appIds)]
-                    )
-
-                    return self.request(betaGroupEndpoint)
-                        .combineLatest(self.request(appsEndpoint))
-                        .map { ($0.0.data, $0.1.data) }
-                        .map {
-                            return BetaTester(
-                                email: tester.attributes?.email,
-                                firstName: tester.attributes?.firstName,
-                                lastName: tester.attributes?.lastName,
-                                inviteType: tester.attributes?.inviteType,
-                                betaGroups: $0,
-                                apps: $1.map { App(bundleId: $0.attributes?.bundleId,
-                                                   name: $0.attributes?.name,
-                                                   primaryLocale: $0.attributes?.primaryLocale,
-                                                   sku: $0.attributes?.sku) }
-                            )
-                        }
-                        .eraseToAnyPublisher()
-
-                case .none:
-                    let tester = BetaTester(
-                        email: tester.attributes?.email,
-                        firstName: tester.attributes?.firstName,
-                        lastName: tester.attributes?.lastName,
-                        inviteType: tester.attributes?.inviteType,
-                        betaGroups: nil,
-                        apps: nil
-                    )
-
-                    return Empty<BetaTester, Error>()
-                        .append(tester)
-                        .eraseToAnyPublisher()
-            }
-        }
-
-        return Publishers.MergeMany(betaTesters).reduce([], { $0 + [$1] }).eraseToAnyPublisher()
-    }
 }
