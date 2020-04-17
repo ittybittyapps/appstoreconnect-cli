666dbf9a33c9964104efc5d740411ea41a653405
Decheng <chinadecheng@gmail.com>
2020-04-17 17:03:42 +1000

    Add function betaGroupIdentifiers and appResourceId

--- Sources/AppStoreConnectCLI/Model/BetaGroup.swift
+++ Sources/AppStoreConnectCLI/Model/BetaGroup.swift
@@ -6,12 +6,15 @@ import Foundation
 
 extension HTTPClient {
     private enum BetaGroupError: LocalizedError {
-        case couldntFindBetaGroup
+        case couldntFindBetaGroup(groupNames: [String])
+        case betaGroupNotUnique(groupNames: [String])
 
         var failureReason: String? {
             switch self {
-                case .couldntFindBetaGroup:
-                    return "Couldn't find beta group with input name or group name not unique"
+                case .couldntFindBetaGroup(let groupNames):
+                    return "Couldn't find beta group with input names \(groupNames)"
+                case .betaGroupNotUnique(let groupNames):
+                    return "The group name you input \(groupNames) are not unique"
             }
         }
     }
@@ -26,11 +29,31 @@ extension HTTPClient {
 
         return self.request(endpoint)
             .tryMap { response throws -> String in
+                guard !response.data.isEmpty else {
+                    throw BetaGroupError.couldntFindBetaGroup(groupNames: [name])
+                }
+
                 guard response.data.count == 1, let id = response.data.first?.id else {
-                    throw BetaGroupError.couldntFindBetaGroup
+                    throw BetaGroupError.betaGroupNotUnique(groupNames: [name])
                 }
 
                 return id
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func betaGroupIdentifiers(matching names: [String]) throws -> AnyPublisher<[String], Error> {
+        let endpoint = APIEndpoint.betaGroups(
+            filter: [ListBetaGroups.Filter.name(names)]
+        )
+
+        return self.request(endpoint)
+            .tryMap { response throws -> [String] in
+                guard !response.data.isEmpty else {
+                    throw BetaGroupError.couldntFindBetaGroup(groupNames: names)
+                }
+
+                return response.data.map { $0.id }
             }
             .eraseToAnyPublisher()
     }
