b988e353924b3921c4b37fc06745a0e8e54dc521
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-04-21 16:44:00 +1000

    Make awaitResult to synchronously wait for a result from a publisher

--- Sources/AppStoreConnectCLI/Helpers/Publisher+Helpers.swift
+++ Sources/AppStoreConnectCLI/Helpers/Publisher+Helpers.swift
@@ -3,6 +3,38 @@
 import Combine
 import Foundation
 
+extension Publisher {
+    func awaitResult() -> Result<[Output], Failure> {
+        var result: Result<[Output], Failure> = .success([])
+
+        let dispatchQueue = DispatchQueue.global(qos: .userInteractive)
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+
+        _ = self
+            .subscribe(on: dispatchQueue)
+            .collect()
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case .failure(let failure):
+                        result = .failure(failure)
+                    }
+
+                    dispatchGroup.leave()
+                },
+                receiveValue: {
+                    result = .success($0)
+                }
+            )
+
+        dispatchGroup.wait()
+
+        return result
+    }
+}
 extension Publisher where Output: ResultRenderable, Failure == Error {
     func renderResult(format: OutputFormat) -> AnyCancellable {
         self.sink(
