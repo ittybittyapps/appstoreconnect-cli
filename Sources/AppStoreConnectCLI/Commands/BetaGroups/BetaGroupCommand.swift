697a506b3f53f5e11b161adb6482c3fbf6e66621
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-04-23 15:39:27 +1000

    Add BetaGroupCommand

---
+++ Sources/AppStoreConnectCLI/Commands/BetaGroups/BetaGroupCommand.swift
@@ -0,0 +1,12 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import ArgumentParser
+import Foundation
+
+struct BetaGroupCommand: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "betagroup",
+        abstract: "",
+        subcommands: []
+    )
+}
