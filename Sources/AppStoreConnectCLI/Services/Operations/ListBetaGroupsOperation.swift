8902c4a36f9514a7566aafdfb2be6ee3e900997e
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-04-28 12:07:04 +1000

    Fully fill out beta group models with group and app data

--- Sources/AppStoreConnectCLI/Services/Operations/ListBetaGroupsOperation.swift
+++ Sources/AppStoreConnectCLI/Services/Operations/ListBetaGroupsOperation.swift
@@ -6,28 +6,39 @@ import Foundation
 
 struct ListBetaGroupsOperation: APIOperation {
     struct ListBetaGroupsDependencies {
+        let apps: (APIEndpoint<AppsResponse>) -> Future<AppsResponse, Error>
         let betaGroups: (APIEndpoint<BetaGroupsResponse>) -> Future<BetaGroupsResponse, Error>
     }
 
     private let endpoint: APIEndpoint<BetaGroupsResponse>
 
     init(options: ListBetaGroupsOptions) {
-        endpoint = .betaGroups()
+        endpoint = .betaGroups(include: [.app])
     }
 
     func execute(with requestor: EndpointRequestor) -> AnyPublisher<[BetaGroup], Error> {
         let response = requestor.request(endpoint)
 
-        let betaGroups = response.map { response in
-            response.data.map { betaGroupData -> BetaGroup in
-                var betaGroup = BetaGroup(appId: "")
-
-                if let attributes = betaGroupData.attributes {
-                    betaGroup.update(with: attributes)
-                }
-
-                return betaGroup
+        let betaGroups = response.flatMap { response -> AnyPublisher<[BetaGroup], Error> in
+            let seed: [String: AppStoreConnect_Swift_SDK.BetaGroup.Attributes] = [:]
+            let betaGroupAttributes = response.data.reduce(into: seed) { result, data in
+                (data.relationships?.app?.data?.id).map { result[$0] = data.attributes }
             }
+
+            let appIds: [String] = Array(betaGroupAttributes.keys)
+            let appsResponse = requestor.request(.apps(filters: [.id(appIds)]))
+
+            let betaGroups = appsResponse.map { appsResponse in
+                appsResponse.data.map { app -> BetaGroup in
+                    let groupAttributes = betaGroupAttributes[app.id]
+                    var betaGroup = BetaGroup(appId: app.id)
+                    betaGroup.update(with: app.attributes)
+                    betaGroup.update(with: groupAttributes)
+                    return betaGroup
+                }
+            }
+
+            return betaGroups.eraseToAnyPublisher()
         }
 
         return betaGroups.eraseToAnyPublisher()
