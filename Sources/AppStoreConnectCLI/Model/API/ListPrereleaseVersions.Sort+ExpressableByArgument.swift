2fc8eadb4aa77aa6ca7e4b9d96a833eba623705c
Oliver Jones <oliver@deeperdesign.com>
2020-04-29 16:20:06 +1000

    Rename files

--- Sources/AppStoreConnectCLI/Model/API/ListPrereleaseVersions.Sort+ExpressableByArgument.swift
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Itty Bitty Apps Pty Ltd
-
-import ArgumentParser
-import AppStoreConnect_Swift_SDK
-import Foundation
-
-extension ListPrereleaseVersions.Sort: Codable, ExpressibleByArgument {
-}
