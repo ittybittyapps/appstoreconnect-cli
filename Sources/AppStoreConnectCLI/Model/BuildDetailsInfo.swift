c70e98cd4249393f2fa7573c466d3735be08ab71
nafisa <nafisa.rahman@ittybittyapps.com>
2020-04-29 11:50:04 +1000

    Added Uses Non Exempt Encryption

--- Sources/AppStoreConnectCLI/Model/BuildDetailsInfo.swift
+++ Sources/AppStoreConnectCLI/Model/BuildDetailsInfo.swift
@@ -10,7 +10,7 @@ import Foundation
 import SwiftyTextTable
 
 struct BuildDetailsInfo: ResultRenderable {
-  let attributes: Build.Attributes?
+  let build: Build?
   let app: AppStoreConnect_Swift_SDK.App?
   let prereleaseVersion: AppStoreConnect_Swift_SDK.PrereleaseVersion?
   let buildBetaDetail: AppStoreConnect_Swift_SDK.BuildBetaDetail?
@@ -20,7 +20,6 @@ struct BuildDetailsInfo: ResultRenderable {
 extension BuildDetailsInfo {
   init(_ build: AppStoreConnect_Swift_SDK.Build ,_ includes: [AppStoreConnect_Swift_SDK.BuildRelationship]?) {
 
-    let attributes = build.attributes
     let relationships = build.relationships
 
     let includedApps = includes?.compactMap { relationship -> AppStoreConnect_Swift_SDK.App? in
@@ -51,13 +50,12 @@ extension BuildDetailsInfo {
         return nil
     }
 
-
     let app = includedApps?.filter { relationships?.app?.data?.id == $0.id }.first
     let prereleaseVersion = includedPrereleaseVersions?.filter { relationships?.preReleaseVersion?.data?.id == $0.id }.first
     let buildBetaDetail = includedBuildBetaDetails?.filter { relationships?.buildBetaDetail?.data?.id == $0.id }.first
     let betaAppReviewSubmission = includedBetaAppReviewSubmissions?.filter { relationships?.betaAppReviewSubmission?.data?.id == $0.id }.first
 
-    self.init(attributes: attributes, app: app, prereleaseVersion: prereleaseVersion, buildBetaDetail: buildBetaDetail, betaAppReviewSubmission: betaAppReviewSubmission)
+    self.init(build: build, app: app, prereleaseVersion: prereleaseVersion, buildBetaDetail: buildBetaDetail, betaAppReviewSubmission: betaAppReviewSubmission)
   }
 }
 
@@ -74,12 +72,13 @@ extension BuildDetailsInfo: TableInfoProvider {
             TextTableColumn(header: "Uploaded Date"),
             TextTableColumn(header: "Expiration Date"),
             TextTableColumn(header: "Expired"),
+            TextTableColumn(header: "Uses Non Exempt Encryption"),
             TextTableColumn(header: "Beta Review state"),
             TextTableColumn(header: "External build state"),
             TextTableColumn(header: "Internal build state"),
             TextTableColumn(header: "Auto Notify"),
 
-            //TextTableColumn(header: "Uses Non Exempt Encryption"), - TODO ??
+
             //TextTableColumn(header: "Beta Groups"), - TODO ??
         ]
     }
@@ -111,14 +110,14 @@ extension BuildDetailsInfo: TableInfoProvider {
 
     var buildAttributes: [CustomStringConvertible] {
        return [
-                attributes?.version,
-                attributes?.processingState,
-                attributes?.minOsVersion,
-                attributes?.uploadedDate?.formattedDate,
-                attributes?.expirationDate?.formattedDate,
-                attributes?.expired?.toYesNo()
+                build?.attributes?.version ?? "",
+                build?.attributes?.processingState ?? "",
+                build?.attributes?.minOsVersion ?? "",
+                build?.attributes?.uploadedDate?.formattedDate ?? "",
+                build?.attributes?.expirationDate?.formattedDate ?? "",
+                build?.attributes?.expired?.toYesNo() ?? "",
+                build?.attributes?.usesNonExemptEncryption?.toYesNo() ?? ""
               ]
-              .map { $0 ?? ""}
     }
 
     var betaAppReviewSubmissionInfo: [CustomStringConvertible] {
