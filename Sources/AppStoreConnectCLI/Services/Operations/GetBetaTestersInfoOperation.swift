58084eef939c0c33b48a70d5a3dbb3dd859f2a9a
Decheng <chinadecheng@gmail.com>
2020-04-30 12:19:25 +1000

    Refactor endpoint and rename GBTInfo, restructure GBTOptions

---
+++ Sources/AppStoreConnectCLI/Services/Operations/GetBetaTestersInfoOperation.swift
@@ -0,0 +1,66 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import AppStoreConnect_Swift_SDK
+import Combine
+import Foundation
+
+struct GetBetaTestersInfoOperation: APIOperation {
+
+    struct GetBetaTesterInfoDependencies {
+        let betaTestersResponse: (APIEndpoint<BetaTestersResponse>) -> Future<BetaTestersResponse, Error>
+    }
+
+    private enum GetTestersError: LocalizedError {
+        case betaTesterNotFound(String)
+
+        var errorDescription: String? {
+            switch self {
+            case .betaTesterNotFound(let email):
+                return "Beta tester with provided email '\(email)' doesn't exist."
+            }
+        }
+    }
+
+    private var endpoint: APIEndpoint<BetaTestersResponse> {
+        var limits: [ListBetaTesters.Limit] = []
+
+        if let limitApps = options.limitApps {
+            limits.append(.apps(limitApps))
+        }
+
+        if let limitBuilds = options.limitBuilds {
+            limits.append(.builds(limitBuilds))
+        }
+
+        if let limitBetaGroups = options.limitBetaGroups {
+            limits.append(.betaGroups(limitBetaGroups))
+        }
+
+        return APIEndpoint.betaTesters(
+            filter: [.email([options.email])],
+            include: [.betaGroups, .apps],
+            limit: limits
+        )
+    }
+
+    let options: GetBetaTesterInfoOptions
+
+    init(options: GetBetaTesterInfoOptions) {
+        self.options = options
+    }
+
+    func execute(with dependencies: GetBetaTesterInfoDependencies) throws -> AnyPublisher<[BetaTester], Error> {
+        dependencies.betaTestersResponse(endpoint)
+            .tryMap { [email = options.email](response: BetaTestersResponse) -> [BetaTester] in
+                guard !response.data.isEmpty else {
+                    throw GetTestersError.betaTesterNotFound(email)
+                }
+
+                return response.data.map {
+                    BetaTester($0, response.included)
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+}
--- Sources/AppStoreConnectCLI/Services/Options/GetBetaTesterInfoOptions.swift
+++ Sources/AppStoreConnectCLI/Services/Options/GetBetaTesterInfoOptions.swift
@@ -1,7 +1,11 @@
 // Copyright 2020 Itty Bitty Apps Pty Ltd
 
+import AppStoreConnect_Swift_SDK
 import Foundation
 
 struct GetBetaTesterInfoOptions {
-    let id: String
+    let email: String
+    var limitApps: Int?
+    var limitBuilds: Int?
+    var limitBetaGroups: Int?
 }
