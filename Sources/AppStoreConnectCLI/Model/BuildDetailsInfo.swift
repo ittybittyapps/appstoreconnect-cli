f9ae681ed9bd32cfdb3c35fe22f2ec52f16f2d45
nafisa <nafisa.rahman@ittybittyapps.com>
2020-05-01 12:23:00 +1000

    Used flat properties

--- Sources/AppStoreConnectCLI/Model/BuildDetailsInfo.swift
+++ Sources/AppStoreConnectCLI/Model/BuildDetailsInfo.swift
@@ -10,14 +10,24 @@ import Foundation
 import SwiftyTextTable
 
 struct BuildDetailsInfo: ResultRenderable {
-  let build: Build?
-  let app: AppStoreConnect_Swift_SDK.App?
-  let prereleaseVersion: AppStoreConnect_Swift_SDK.PrereleaseVersion?
-  let buildBetaDetail: AppStoreConnect_Swift_SDK.BuildBetaDetail?
-  let betaAppReviewSubmission: AppStoreConnect_Swift_SDK.BetaAppReviewSubmission?
+  let app: App?
+  let platform: String?
+  let version: String?
+  let externalBuildState: String?
+  let internalBuildState: String?
+  let autoNotifyEnabled: String?
+  let buildNumber: String?
+  let processingState: String?
+  let minOsVersion: String?
+  let uploadedDate: String?
+  let expirationDate: String?
+  let expired: String?
+  let usesNonExemptEncryption: String?
+  let betaReviewState: String?
 }
 
 extension BuildDetailsInfo {
+
   init(_ build: AppStoreConnect_Swift_SDK.Build ,_ includes: [AppStoreConnect_Swift_SDK.BuildRelationship]?) {
 
     let relationships = build.relationships
@@ -50,95 +60,74 @@ extension BuildDetailsInfo {
         return nil
     }
 
-    let app = includedApps?.filter { relationships?.app?.data?.id == $0.id }.first
+    let appDetails = includedApps?.filter { relationships?.app?.data?.id == $0.id }.first
     let prereleaseVersion = includedPrereleaseVersions?.filter { relationships?.preReleaseVersion?.data?.id == $0.id }.first
     let buildBetaDetail = includedBuildBetaDetails?.filter { relationships?.buildBetaDetail?.data?.id == $0.id }.first
     let betaAppReviewSubmission = includedBetaAppReviewSubmissions?.filter { relationships?.betaAppReviewSubmission?.data?.id == $0.id }.first
 
+    let app = App(
+                    bundleId: appDetails?.attributes?.bundleId,
+                    name: appDetails?.attributes?.name
+                  )
+
     self.init(
-              build: build,
-              app: app,
-              prereleaseVersion: prereleaseVersion,
-              buildBetaDetail: buildBetaDetail,
-              betaAppReviewSubmission: betaAppReviewSubmission
-             )
+                app: app,
+                platform: prereleaseVersion?.attributes?.platform?.rawValue,
+                version: prereleaseVersion?.attributes?.version,
+                externalBuildState: buildBetaDetail?.attributes?.externalBuildState?.rawValue,
+                internalBuildState: buildBetaDetail?.attributes?.internalBuildState?.rawValue,
+                autoNotifyEnabled: buildBetaDetail?.attributes?.autoNotifyEnabled?.toYesNo(),
+                buildNumber: build.attributes?.version,
+                processingState: build.attributes?.processingState,
+                minOsVersion: build.attributes?.minOsVersion,
+                uploadedDate: build.attributes?.uploadedDate?.formattedDate,
+                expirationDate: build.attributes?.expirationDate?.formattedDate,
+                expired: build.attributes?.expired?.toYesNo(),
+                usesNonExemptEncryption: build.attributes?.usesNonExemptEncryption?.toYesNo(),
+                betaReviewState: betaAppReviewSubmission?.attributes?.betaReviewState?.rawValue
+              )
   }
 }
 
 extension BuildDetailsInfo: TableInfoProvider {
     static func tableColumns() -> [TextTableColumn] {
        return [
-            TextTableColumn(header: "Bundle Id"),
-            TextTableColumn(header: "App Name"),
-            TextTableColumn(header: "Platform"),
-            TextTableColumn(header: "Version"),
-            TextTableColumn(header: "Build Number"),
-            TextTableColumn(header: "Processing State"),
-            TextTableColumn(header: "Min OS Version"),
-            TextTableColumn(header: "Uploaded Date"),
-            TextTableColumn(header: "Expiration Date"),
-            TextTableColumn(header: "Expired"),
-            TextTableColumn(header: "Uses Non Exempt Encryption"),
-            TextTableColumn(header: "Beta Review state"),
-            TextTableColumn(header: "External build state"),
-            TextTableColumn(header: "Internal build state"),
-            TextTableColumn(header: "Auto Notify")
-        ]
-    }
-
-    var appInfo: [CustomStringConvertible] {
-        return [
-                app?.attributes?.bundleId,
-                app?.attributes?.name
-               ]
-               .map {$0 ?? ""}
-      }
-
-    var prereleaseVersionInfo: [CustomStringConvertible] {
-        return [
-                prereleaseVersion?.attributes?.platform?.rawValue ,
-                prereleaseVersion?.attributes?.version
-               ]
-               .map {$0 ?? ""}
-    }
-
-    var buildBetaDetailInfo: [CustomStringConvertible] {
-        return [
-                buildBetaDetail?.attributes?.externalBuildState?.rawValue,
-                buildBetaDetail?.attributes?.internalBuildState?.rawValue ,
-                buildBetaDetail?.attributes?.autoNotifyEnabled?.toYesNo(),
-               ]
-               .map { $0 ?? ""}
-    }
-
-    var buildAttributes: [CustomStringConvertible] {
-       return [
-                build?.attributes?.version ?? "",
-                build?.attributes?.processingState ?? "",
-                build?.attributes?.minOsVersion ?? "",
-                build?.attributes?.uploadedDate?.formattedDate ?? "",
-                build?.attributes?.expirationDate?.formattedDate ?? "",
-                build?.attributes?.expired?.toYesNo() ?? "",
-                build?.attributes?.usesNonExemptEncryption?.toYesNo() ?? ""
+                TextTableColumn(header: "Bundle Id"),
+                TextTableColumn(header: "App Name"),
+                TextTableColumn(header: "Platform"),
+                TextTableColumn(header: "Version"),
+                TextTableColumn(header: "Build Number"),
+                TextTableColumn(header: "Beta Review state"),
+                TextTableColumn(header: "Processing State"),
+                TextTableColumn(header: "External build state"),
+                TextTableColumn(header: "Internal build state"),
+                TextTableColumn(header: "Auto Notify"),
+                TextTableColumn(header: "Min OS Version"),
+                TextTableColumn(header: "Uploaded Date"),
+                TextTableColumn(header: "Expiration Date"),
+                TextTableColumn(header: "Expired"),
+                TextTableColumn(header: "Uses Non Exempt Encryption")
               ]
-    }
-
-    var betaAppReviewSubmissionInfo: [CustomStringConvertible] {
-        return [
-                betaAppReviewSubmission?.attributes?.betaReviewState?.rawValue
-               ]
-               .map { $0 ?? ""}
     }
 
     var tableRow: [CustomStringConvertible]  {
         return [
-                appInfo.map {$0},
-                prereleaseVersionInfo.map {$0},
-                buildAttributes.map {$0},
-                betaAppReviewSubmissionInfo.map {$0},
-                buildBetaDetailInfo.map {$0}
+                app?.bundleId,
+                app?.name,
+                platform,
+                version,
+                buildNumber,
+                betaReviewState,
+                processingState,
+                externalBuildState,
+                internalBuildState,
+                autoNotifyEnabled,
+                minOsVersion,
+                uploadedDate,
+                expirationDate,
+                expired,
+                usesNonExemptEncryption
                ]
-               .flatMap{$0}
+              .map { $0 ?? ""}
     }
 }
-
