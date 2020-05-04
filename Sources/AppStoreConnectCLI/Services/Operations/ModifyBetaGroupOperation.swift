dec2b3367a78eaddfe5d0732093818cf0058136e
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-04 16:37:05 +1000

    Amend ModifyBetaGroupOperation options to not use internal id

--- Sources/AppStoreConnectCLI/Services/Operations/ModifyBetaGroupOperation.swift
+++ Sources/AppStoreConnectCLI/Services/Operations/ModifyBetaGroupOperation.swift
@@ -6,11 +6,26 @@ import Foundation
 
 struct ModifyBetaGroupOperation: APIOperation {
     struct Options {
-        let id: String
-        let name: String?
+        let appId: String
+        let currentGroupName: String
+        let newGroupName: String?
         let publicLinkEnabled: Bool?
         let publicLinkLimit: Int?
         let publicLinkLimitEnabled: Bool?
+    }
+
+    enum Error: LocalizedError {
+        case betaGroupNotFound(groupName: String, appId: String)
+        case betaGroupNotUniqueToApp(groupName: String, appId: String)
+
+        var errorDescription: String? {
+            switch self {
+            case .betaGroupNotFound(let groupName, let appId):
+                return "No beta group found with name: \(groupName) and app id: \(appId)"
+            case .betaGroupNotUniqueToApp(let groupName, let appId):
+                return "Multiple beta groups found with name: \(groupName) and app id: \(appId)"
+            }
+        }
     }
 
     typealias BetaGroup = AppStoreConnect_Swift_SDK.BetaGroup
@@ -21,10 +36,28 @@ struct ModifyBetaGroupOperation: APIOperation {
         self.options = options
     }
 
-    func execute(with requestor: EndpointRequestor) throws -> AnyPublisher<BetaGroup, Error> {
+    func execute(with requestor: EndpointRequestor) throws -> AnyPublisher<BetaGroup, Swift.Error> {
+        let groupName = options.currentGroupName
+        let appId = options.appId
+
+        let betaGroupsEndpoint = APIEndpoint.betaGroups(filter: [.app([appId]), .name([groupName])])
+        let betaGroups = try requestor.request(betaGroupsEndpoint).await().data
+
+        guard betaGroups.count == 1, let betaGroup = betaGroups.first else {
+            let groupName = options.currentGroupName
+            let appId = options.appId
+
+            switch betaGroups.count {
+            case 0:
+                throw Error.betaGroupNotFound(groupName: groupName, appId: appId)
+            default:
+                throw Error.betaGroupNotUniqueToApp(groupName: groupName, appId: appId)
+            }
+        }
+
         let endpoint: APIEndpoint<BetaGroupResponse> = .modify(
-            betaGroupWithId: options.id,
-            name: options.name,
+            betaGroupWithId: betaGroup.id,
+            name: options.newGroupName,
             publicLinkEnabled: options.publicLinkEnabled,
             publicLinkLimit: options.publicLinkLimit,
             publicLinkLimitEnabled: options.publicLinkLimitEnabled
