bef7cfc0cb6cfa09f0076b1a41d112283230b0fa
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-05 15:29:26 +1000

    Add DeleteBetaGroupCommand

---
+++ Sources/AppStoreConnectCLI/Commands/TestFlight/BetaGroups/DeleteBetaGroupCommand.swift
@@ -0,0 +1,35 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import ArgumentParser
+import Foundation
+
+struct DeleteBetaGroupCommand: CommonParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "delete",
+        abstract: "Delete a beta group"
+    )
+
+    @OptionGroup()
+    var common: CommonOptions
+
+    @Argument(
+        help: """
+        The reverse-DNS bundle ID of the app which the group is associated with. \
+        Must be unique. (eg. com.example.app)
+        """
+    ) var appBundleId: String
+
+    @Argument(
+        help: ArgumentHelp(
+            "The name of the beta group to be deleted",
+            discussion: """
+            This name will be used to search for a unique beta group matching the specified \
+            app bundle id
+            """,
+            valueName: "beta-group-name"
+        )
+    ) var betaGroupName: String
+
+    func run() throws {
+    }
+}
