7ae454479054e99f43beae30facb96b23c9c9140
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-06 17:21:28 +1000

    Ensure GetBetaGroupOperation results exactly match the provided name

--- Sources/AppStoreConnectCLI/Services/Operations/GetBetaGroupOperation.swift
+++ Sources/AppStoreConnectCLI/Services/Operations/GetBetaGroupOperation.swift
@@ -42,12 +42,14 @@ struct GetBetaGroupOperation: APIOperation {
         )
 
         let betaGroup = requestor.request(endpoint).tryMap { response -> BetaGroup in
-            switch response.data.first {
-            case .some(let betaGroup) where response.data.count == 1:
+            let betaGroups = response.data.filter { $0.attributes?.name == betaGroupName }
+            
+            switch (betaGroups.first, betaGroups.count) {
+            case (.some(let betaGroup), 1):
                 return betaGroup
-            case .some:
+            case (.some, _):
                 throw Error.betaGroupNotUniqueToApp(groupName: betaGroupName, bundleId: bundleId)
-            case .none:
+            case (.none, _):
                 throw Error.betaGroupNotFound(groupName: betaGroupName, bundleId: bundleId)
             }
         }
--- Tests/appstoreconnect-cliTests/Operations/GetBetaGroupOperationTests.swift
+++ Tests/appstoreconnect-cliTests/Operations/GetBetaGroupOperationTests.swift
@@ -37,7 +37,16 @@ final class GetBetaGroupOperationTests: XCTestCase {
 
     var betaGroup: BetaGroup {
         BetaGroup(
-            attributes: nil,
+            attributes: BetaGroup.Attributes(
+                isInternalGroup: false,
+                name: "Some Group",
+                publicLink: nil,
+                publicLinkEnabled: nil,
+                publicLinkId: nil,
+                publicLinkLimit: nil,
+                publicLinkLimitEnabled: nil,
+                createdDate: nil
+            ),
             id: "1234",
             relationships: nil,
             links: ResourceLinks(self: testUrl)
@@ -70,6 +79,48 @@ final class GetBetaGroupOperationTests: XCTestCase {
             response: { (endpoint: APIEndpoint<BetaGroupsResponse>) -> Future<BetaGroupsResponse, Error> in
                 let response = BetaGroupsResponse(
                     data: [],
+                    included: nil,
+                    links: PagedDocumentLinks(first: nil, next: nil, self: self.testUrl),
+                    meta: nil
+                )
+
+                return Future { $0(.success(response)) }
+            }
+        )
+
+        let result = Result {
+            try Operation(options: options).execute(with: betaGroupNotFoundRequestor).await()
+        }
+
+        switch result {
+        case .failure(Operation.Error.betaGroupNotFound(groupName: "Some Group", bundleId: "com.example.test")):
+            break
+        default:
+            XCTFail()
+        }
+    }
+
+    func testBetaGroupNotFoundWithDifferentName() {
+        let betaGroup = BetaGroup(
+            attributes: BetaGroup.Attributes(
+                isInternalGroup: false,
+                name: "Just Some Group",
+                publicLink: nil,
+                publicLinkEnabled: nil,
+                publicLinkId: nil,
+                publicLinkLimit: nil,
+                publicLinkLimitEnabled: nil,
+                createdDate: nil
+            ),
+            id: "5678",
+            relationships: nil,
+            links: ResourceLinks(self: testUrl)
+        )
+
+        let betaGroupNotFoundRequestor = OneEndpointTestRequestor(
+            response: { (endpoint: APIEndpoint<BetaGroupsResponse>) -> Future<BetaGroupsResponse, Error> in
+                let response = BetaGroupsResponse(
+                    data: [betaGroup],
                     included: nil,
                     links: PagedDocumentLinks(first: nil, next: nil, self: self.testUrl),
                     meta: nil
