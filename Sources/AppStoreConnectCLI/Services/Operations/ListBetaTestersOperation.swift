6e9d2269d182768ccef5d564a9b492c285d95d2b
Decheng <chinadecheng@gmail.com>
2020-05-07 10:48:52 +1000

    Move computed var in ListBetaTestersOperation to func execute

--- Sources/AppStoreConnectCLI/Services/Operations/ListBetaTestersOperation.swift
+++ Sources/AppStoreConnectCLI/Services/Operations/ListBetaTestersOperation.swift
@@ -29,70 +29,6 @@ struct ListBetaTestersOperation: APIOperation {
         }
     }
 
-    var limits: [ListBetaTesters.Limit] {
-        var limits: [ListBetaTesters.Limit] = []
-
-        if let resourcesLimit = options.relatedResourcesLimit {
-            limits.append(.apps(resourcesLimit))
-            limits.append(.betaGroups(resourcesLimit))
-        }
-
-        if let limit = options.limit {
-            limits.append(.betaTesters(limit))
-        }
-
-        return limits
-    }
-
-    var sorts: [ListBetaTesters.Sort] {
-        var sorts: [ListBetaTesters.Sort] = []
-
-        if let sort = options.sort {
-            sorts.append(sort)
-        }
-
-        return sorts
-    }
-
-    var filters: [ListBetaTesters.Filter] {
-        var filters: [ListBetaTesters.Filter] = []
-
-        if let firstName = options.firstName {
-            filters.append(.firstName([firstName]))
-        }
-
-        if let lastName = options.lastName {
-            filters.append(.lastName([lastName]))
-        }
-
-        if let email = options.email {
-            filters.append(.email([email]))
-        }
-
-        if let inviteType = options.inviteType {
-            filters.append(.inviteType([inviteType.rawValue]))
-        }
-
-        if let appIds = options.appIds {
-            filters.append(.apps(appIds))
-        }
-
-        if let groupIds = options.groupIds {
-            filters.append(.betaGroups(groupIds))
-        }
-
-        return filters
-    }
-
-    var endpoint: APIEndpoint<BetaTestersResponse> {
-        .betaTesters(
-            filter: filters,
-            include: [.apps, .betaGroups],
-            limit: limits,
-            sort: sorts
-        )
-    }
-
     private let options: Options
 
     typealias Output = [GetBetaTesterOperation.Output]
@@ -102,7 +38,71 @@ struct ListBetaTestersOperation: APIOperation {
     }
 
     func execute(with requestor: EndpointRequestor) throws -> AnyPublisher<Output, Swift.Error> {
-        requestor.request(endpoint)
+        var limits: [ListBetaTesters.Limit] {
+            var limits: [ListBetaTesters.Limit] = []
+
+            if let resourcesLimit = options.relatedResourcesLimit {
+                limits.append(.apps(resourcesLimit))
+                limits.append(.betaGroups(resourcesLimit))
+            }
+
+            if let limit = options.limit {
+                limits.append(.betaTesters(limit))
+            }
+
+            return limits
+        }
+
+        var sorts: [ListBetaTesters.Sort] {
+            var sorts: [ListBetaTesters.Sort] = []
+
+            if let sort = options.sort {
+                sorts.append(sort)
+            }
+
+            return sorts
+        }
+
+        var filters: [ListBetaTesters.Filter] {
+            var filters: [ListBetaTesters.Filter] = []
+
+            if let firstName = options.firstName {
+                filters.append(.firstName([firstName]))
+            }
+
+            if let lastName = options.lastName {
+                filters.append(.lastName([lastName]))
+            }
+
+            if let email = options.email {
+                filters.append(.email([email]))
+            }
+
+            if let inviteType = options.inviteType {
+                filters.append(.inviteType([inviteType.rawValue]))
+            }
+
+            if let appIds = options.appIds, !appIds.isEmpty {
+                filters.append(.apps(appIds))
+            }
+
+            if let groupIds = options.groupIds, !groupIds.isEmpty {
+                filters.append(.betaGroups(groupIds))
+            }
+
+            return filters
+        }
+
+        var endpoint: APIEndpoint<BetaTestersResponse> {
+            .betaTesters(
+                filter: filters,
+                include: [.apps, .betaGroups],
+                limit: limits,
+                sort: sorts
+            )
+        }
+
+        return requestor.request(endpoint)
             .tryMap { (response: BetaTestersResponse) -> Output in
                 guard !response.data.isEmpty else {
                     throw Error.notFound
