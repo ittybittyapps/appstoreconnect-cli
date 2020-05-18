d7fe2e525e12c7bcd8623773831ed41e264e18bd
nafisa <nafisa.rahman@ittybittyapps.com>
2020-05-18 17:57:38 +1000

    wip test

--- Tests/appstoreconnect-cliTests/Operations/ListPreReleaseVersionsOperationTests.swift
+++ Tests/appstoreconnect-cliTests/Operations/ListPreReleaseVersionsOperationTests.swift
@@ -7,171 +7,71 @@ import Combine
 import XCTest
 
 final class ListPreReleaseVersionsOperationTests: XCTestCase {
-    typealias Dependencies = ListPreReleaseVersionsOperation.Dependencies
+    typealias Operation = ListPreReleaseVersionsOperation
+    typealias Options = Operation.Options
 
-    func testReturnsNoVersions() throws {
-        let value = try ListPreReleaseVersionsOperation(options: .init())
-            .execute(with: .makeDependency(Fixture.noDataResponse))
-            .await()
-
-        XCTAssertTrue(value.isEmpty)
-    }
+    let successRequestor = OneEndpointTestRequestor(
+        response: { _ in Future({ $0(.success(dataResponse)) }) }
+    )
 
     func testReturnsOnePreReleaseVersion() throws {
-        let value = try ListPreReleaseVersionsOperation(options: .init())
-            .execute(with: .makeDependency(Fixture.dataResponse))
-            .await()
-
-        XCTAssertEqual(value.count, 1)
+        let operation = Operation(options: Options(filterAppIds: [], filterVersions: [], filterPlatforms: [], sort: nil))
+        let output = try operation.execute(with: successRequestor).await()
+        XCTAssertEqual(output.count, 1)
+        XCTAssertEqual(output.first?.preReleaseVersion.attributes?.version, "1.0")
     }
-}
 
-private enum Fixture {
-    static let noAppDataResponse = """
-    {
-      "data" : [ ],
-      "links" : {
-        "self" : "https://api.appstoreconnect.apple.com/v1/apps"
-      },
-      "meta" : {
-        "paging" : {
-          "total" : 0,
-          "limit" : 50
-        }
-      }
-    }
-    """.data(using: .utf8)!
-
-    static let noDataResponse = """
-    {
-      "data" : [ ],
-      "links" : {
-        "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions?include=app"
-      },
-      "meta" : {
-        "paging" : {
-          "total" : 0,
-          "limit" : 50
-        }
-      }
-    }
-    """.data(using: .utf8)!
-
-    static let dataResponse = """
-    {
-      "data" : [ {
-        "type" : "preReleaseVersions",
-        "id" : "12345678-abcd-abcd-efab-1234567890ab",
-        "attributes" : {
-          "version" : "1.0",
-          "platform" : "IOS"
-        },
-        "relationships" : {
-          "builds" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/relationships/builds",
-              "related" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/builds"
-            }
-          },
-          "app" : {
-            "data" : {
-              "type" : "apps",
-              "id" : "1234567890"
+    // TODO: return only included apps in JSON 
+    static let dataResponse: PrereleaseVersionResponse = """
+        {
+          "data" : [ {
+            "type" : "preReleaseVersions",
+            "id" : "12345678-abcd-abcd-efab-1234567890ab",
+            "attributes" : {
+              "version" : "1.0",
+              "platform" : "IOS"
             },
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/relationships/app",
-              "related" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/app"
-            }
-          }
-        },
-        "links" : {
-          "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab"
-        }
-      } ],
-      "included" : [ {
-        "type" : "apps",
-        "id" : "1234567890",
-        "attributes" : {
-          "name" : "A Test App",
-          "bundleId" : "com.example.App",
-          "sku" : "EXAMPLEAPP1",
-          "primaryLocale" : "en-AU"
-        },
-        "relationships" : {
-          "betaTesters" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/betaTesters",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/betaTesters"
-            }
-          },
-          "betaGroups" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/betaGroups",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/betaGroups"
-            }
-          },
-          "preReleaseVersions" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/preReleaseVersions",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/preReleaseVersions"
-            }
-          },
-          "betaAppLocalizations" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/betaAppLocalizations",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/betaAppLocalizations"
-            }
-          },
-          "builds" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/builds",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/builds"
-            }
-          },
-          "betaLicenseAgreement" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/betaLicenseAgreement",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/betaLicenseAgreement"
-            }
-          },
-          "betaAppReviewDetail" : {
-            "links" : {
-              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/relationships/betaAppReviewDetail",
-              "related" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890/betaAppReviewDetail"
-            }
-          }
-        },
-        "links" : {
-          "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890"
-        }
-      } ],
-      "links" : {
-        "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions?include=app&filter%5Bapp%5D=1234567890"
-      },
-      "meta" : {
-        "paging" : {
-          "total" : 1,
-          "limit" : 50
-        }
-      }
-    }
-    """.data(using: .utf8)!
-}
-
-private extension ListPreReleaseVersionsOperation.Dependencies {
-    static let jsonDecoder = JSONDecoder()
-
-    static func makeDependency(_ data: Data) -> Self {
-        Self(
-            preReleaseVersions: { _ in
-                Future<PreReleaseVersionsResponse, Error> { promise in
-                    let result = try! jsonDecoder.decode(PreReleaseVersionsResponse.self, from: data)
-                    promise(.success(result))
+            "relationships" : {
+              "app" : {
+                "data" : {
+                  "type" : "apps",
+                  "id" : "1234567890"
+                },
+                "links" : {
+                  "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/relationships/app",
+                  "related" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab/app"
                 }
+              }
             },
-            apps: { _ in
-                fatalError("Shouldn't execute in test")
+            "links" : {
+              "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions/12345678-abcd-abcd-efab-1234567890ab"
             }
-        )
-    }
+          } ],
+          "included" : [ {
+            "type" : "apps",
+            "id" : "1234567890",
+            "attributes" : {
+              "name" : "A Test App",
+              "bundleId" : "com.example.App",
+              "sku" : "EXAMPLEAPP1",
+              "primaryLocale" : "en-AU"
+            },
+            "links" : {
+              "self" : "https://api.appstoreconnect.apple.com/v1/apps/1234567890"
+            }
+          } ],
+          "links" : {
+            "self" : "https://api.appstoreconnect.apple.com/v1/preReleaseVersions?include=app"
+          },
+          "meta" : {
+            "paging" : {
+              "total" : 1,
+              "limit" : 50
+            }
+          }
+        }
+        """
+        .data(using: .utf8)
+        .map({ try! jsonDecoder.decode(PrereleaseVersionResponse.self, from: $0) })!
 }
+
