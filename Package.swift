d7d49bee77554a80a462d33c81743992c08acf7e
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-20 18:37:55 +1000

    Move BetaGroup model into Model module

--- Package.swift
+++ Package.swift
@@ -44,9 +44,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "appstoreconnect-cli",
-            dependencies: ["AppStoreConnectCLI"]),
+        .target(name: "appstoreconnect-cli", dependencies: ["AppStoreConnectCLI"]),
         .target(
             name: "AppStoreConnectCLI",
             dependencies: [
@@ -59,8 +57,6 @@ let package = Package(
                 .product(name: "CodableCSV", package: "CodableCSV")
             ]
         ),
-        .testTarget(
-            name: "appstoreconnect-cliTests",
-            dependencies: ["appstoreconnect-cli"]),
+        .testTarget(name: "appstoreconnect-cliTests", dependencies: ["appstoreconnect-cli"]),
     ]
 )
