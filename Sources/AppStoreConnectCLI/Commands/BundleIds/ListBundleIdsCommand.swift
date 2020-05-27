60d229b876813e66e3c43fd7fbc78a18850d8f61
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-27 18:03:13 +1000

    Update ListBundleIdsCommand to use the service function

--- Sources/AppStoreConnectCLI/Commands/BundleIds/ListBundleIdsCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/BundleIds/ListBundleIdsCommand.swift
@@ -2,8 +2,6 @@
 
 import AppStoreConnect_Swift_SDK
 import ArgumentParser
-import Foundation
-import struct Model.BundleId
 
 struct ListBundleIdsCommand: CommonParsableCommand {
     public static var configuration = CommandConfiguration(
@@ -17,64 +15,32 @@ struct ListBundleIdsCommand: CommonParsableCommand {
     @Option(help: "Limit the number of resources (maximum 200).")
     var limit: Int?
 
-    @OptionGroup()
-    var filters: FilterOptions
+    @Option(parsing: .upToNextOption, help: "Filter the results by reverse-DNS bundle ID identifier (eg. com.example.app)")
+    var filterIdentifier: [String]
+
+    @Option(parsing: .upToNextOption, help: "Filter the results by app name")
+    var filterName: [String]
+
+    @Option(
+        parsing: .upToNextOption,
+        help: "Filter the results by platform (\(Platform.allCases.description))."
+    )
+    var filterPlatform: [String]
+
+    @Option(parsing: .upToNextOption, help: "Filter the results by seed ID")
+    var filterSeedId: [String]
 
     func run() throws {
         let service = try makeService()
 
-        let request = APIEndpoint.listBundleIds(
-            filter: [BundleIds.Filter](filters),
+        let bundleIds = try service.listBundleIds(
+            identifiers: filterIdentifier,
+            names: filterName,
+            platforms: filterPlatform,
+            seedIds: filterSeedId,
             limit: limit
         )
 
-        let bundleId = try service.request(request)
-            .map { $0.data.map(BundleId.init) }
-            .await()
-
-        bundleId.render(format: common.outputFormat)
-    }
-}
-
-extension ListBundleIdsCommand {
-    struct FilterOptions: ParsableArguments {
-        @Option(parsing: .upToNextOption, help: "Filter the results by reverse-DNS bundle ID identifier (eg. com.example.app)")
-        var filterIdentifier: [String]
-
-        @Option(parsing: .upToNextOption, help: "Filter the results by app name")
-        var filterName: [String]
-
-        @Option(
-            parsing: .upToNextOption,
-            help: "Filter the results by platform (\(Platform.allCases.description))."
-        )
-        var filterPlatform: [Platform]
-
-        @Option(parsing: .upToNextOption, help: "Filter the results by seed ID")
-        var filterSeedId: [String]
-    }
-}
-
-extension Array where Element == BundleIds.Filter {
-    init?(_ options: ListBundleIdsCommand.FilterOptions) {
-        var filters = [Element]()
-
-        if options.filterIdentifier.isEmpty == false {
-            filters.append(.identifier(options.filterIdentifier))
-        }
-
-        if options.filterName.isEmpty == false {
-            filters.append(.name(options.filterName))
-        }
-
-        if options.filterPlatform.isEmpty == false {
-            filters.append(.platform(options.filterPlatform))
-        }
-
-        if options.filterSeedId.isEmpty == false {
-            filters.append(.seedId(options.filterSeedId))
-        }
-
-        self.init(filters)
+        bundleIds.render(format: common.outputFormat)
     }
 }
