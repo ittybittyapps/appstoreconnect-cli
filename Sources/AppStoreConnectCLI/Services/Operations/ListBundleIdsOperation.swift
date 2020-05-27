14c47f6c52c68455a8fce5e245fc4a639b8938a4
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-05-27 17:54:47 +1000

    Fill out ListBundleIdsOperation

--- Sources/AppStoreConnectCLI/Services/Operations/ListBundleIdsOperation.swift
+++ Sources/AppStoreConnectCLI/Services/Operations/ListBundleIdsOperation.swift
@@ -5,6 +5,33 @@ import Combine
 
 struct ListBundleIdsOperation: APIOperation {
     struct Options {
+        let identifiers: [String]
+        let names: [String]
+        let platforms: [String]
+        let seedIds: [String]
+        let limit: Int?
+
+        fileprivate var endpoint: APIEndpoint<BundleIdsResponse> {
+            var filters: [BundleIds.Filter] = []
+
+            if identifiers.isEmpty == false {
+                filters.append(.identifier(identifiers))
+            }
+
+            if names.isEmpty == false {
+                filters.append(.name(names))
+            }
+
+            if platforms.isEmpty == false {
+                filters.append(.platform(platforms.compactMap(Platform.init(rawValue:))))
+            }
+
+            if seedIds.isEmpty == false {
+                filters.append(.seedId(seedIds))
+            }
+
+            return .listBundleIds(filter: filters, limit: limit)
+        }
     }
 
     private let options: Options
@@ -16,6 +43,6 @@ struct ListBundleIdsOperation: APIOperation {
     typealias BundleId = AppStoreConnect_Swift_SDK.BundleId
 
     func execute(with requestor: EndpointRequestor) throws -> AnyPublisher<[BundleId], Error> {
-        Empty().eraseToAnyPublisher()
+        requestor.request(options.endpoint).map(\.data).eraseToAnyPublisher()
     }
 }
