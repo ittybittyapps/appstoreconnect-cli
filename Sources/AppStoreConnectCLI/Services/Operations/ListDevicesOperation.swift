8e1a7994b8ea88d9a888db6fdfc8800ff622f0f5
Decheng <chinadecheng@gmail.com>
2020-06-01 11:09:25 +1000

    Create ListDevicesOperation and listDevices service function

---
+++ Sources/AppStoreConnectCLI/Services/Operations/ListDevicesOperation.swift
@@ -0,0 +1,63 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import AppStoreConnect_Swift_SDK
+import Combine
+
+struct ListDevicesOperation: APIOperation {
+
+    struct Options {
+       let filterName: [String]
+       let filterPlatform: [Platform]
+       let filterUDID: [String]
+       let filterStatus: DeviceStatus?
+       let sort: Devices.Sort?
+       let limit: Int?
+    }
+
+    private let options: Options
+
+    init(options: Options) {
+        self.options = options
+    }
+
+    func execute(with requestor: EndpointRequestor) throws -> AnyPublisher<[Device], Error> {
+
+        var filters = [Devices.Filter]()
+
+        if !options.filterName.isEmpty {
+            filters.append(.name(options.filterName))
+        }
+
+        if !options.filterPlatform.isEmpty {
+            // API Device attributes use the BundleIdPlatform enum,
+            // rather than a Platform, so there is no support for
+            // tvOs or watchOs.
+            // This appears to be an API issue.
+            filters.append(.platform(options.filterPlatform))
+        }
+
+        if !options.filterUDID.isEmpty {
+            filters.append(.udid(options.filterUDID))
+        }
+
+        if let filterStatus = options.filterStatus {
+            filters.append(.status([filterStatus]))
+        }
+
+        let sort = [options.sort].compactMap { $0 }
+        let limit = options.limit
+
+        return requestor.requestAllPages {
+                .listDevices(
+                    filter: filters,
+                    sort: sort,
+                    limit: limit,
+                    next: $0
+                )
+            }
+            .map { $0.flatMap { $0.data } }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension DevicesResponse: PaginatedResponse { }
