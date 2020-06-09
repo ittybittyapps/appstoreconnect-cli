b895f4f5bd5cbdc45be988222f2a98a64fa2dcc5
Decheng <chinadecheng@gmail.com>
2020-06-09 12:30:33 +1000

    Implement DisableBundleIdCapabilityCommand

---
+++ Sources/AppStoreConnectCLI/Commands/Capability/DisableBundleIdCapabilityCommand.swift
@@ -0,0 +1,27 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import AppStoreConnect_Swift_SDK
+import ArgumentParser
+
+struct DisableBundleIdCapabilityCommand: CommonParsableCommand {
+
+    public static var configuration = CommandConfiguration(
+        commandName: "disable",
+        abstract: "Disable a capability for a bundle ID."
+    )
+
+    @OptionGroup()
+    var common: CommonOptions
+
+    @Argument(help: "The reverse-DNS bundle ID identifier to delete. Must be unique. (eg. com.example.app)")
+    var bundleId: String
+
+    @Argument(help: "Bundle Id capability type. \n \(CapabilityType.allCases)")
+    var capabilityType: CapabilityType
+
+    func run() throws {
+        let service = try makeService()
+
+        try service.disableBundleIdCapability(bundleId: bundleId, capabilityType: capabilityType)
+    }
+}
