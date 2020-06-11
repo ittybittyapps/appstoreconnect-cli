701f95e9109115cf6c2d709217043136f1f9f47e
Decheng <chinadecheng@gmail.com>
2020-06-11 17:43:32 +1000

    Implement basic pull beta groups command

---
+++ Sources/AppStoreConnectCLI/Commands/TestFlight/BetaGroups/Sync/SyncBetaGroupsCommand.swift
@@ -0,0 +1,17 @@
+// Copyright 2020 Itty Bitty Apps Pty Ltd
+
+import ArgumentParser
+
+struct SyncBetaGroupsCommand: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "sync",
+        abstract: """
+        Sync information about beta groups with provided configuration file.
+        """,
+        subcommands: [
+            PullBetaGroupsCommand.self,
+            PushBetaGroupsCommand.self,
+        ],
+        defaultSubcommand: PullBetaGroupsCommand.self
+    )
+}
