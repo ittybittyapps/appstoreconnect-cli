e8cc80148f61052dfbe3e5cad36259f6a5901108
Decheng <chinadecheng@gmail.com>
2020-06-19 15:10:50 +1000

    Add ReportFilter for restricting inputs in download sales command

--- Sources/AppStoreConnectCLI/Commands/Reports/DownloadSalesAndTrendsReportsCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Reports/DownloadSalesAndTrendsReportsCommand.swift
@@ -6,7 +6,84 @@ import FileSystem
 
 struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
 
-    typealias Filter = DownloadSalesAndTrendsReports.Filter
+    // swiftlint:disable identifier_name
+    enum ReportFilter: String, ExpressibleByArgument, CaseIterable, CustomStringConvertible {
+
+        // SALES
+        case sales_daily_summary
+        case sales_weekly_summary
+        case sales_monthly_summary
+        case sales_yearly_summary
+        case sales_weekly_opt_in
+
+        // SUBSCRIPTION
+        case subscription_daily_summary
+        case subscription_event_daily_summary
+
+        // SUBSCRIBER
+        case subscriber_daily_detailed
+
+        // NEWSSTAND
+        case newsstand_daily_detailed
+        case newsstand_weekly_detailed
+
+        // PRE_ORDER
+        case pre_order_daily_summary
+        case pre_order_weekly_summary
+        case pre_order_monthly_summary
+        case pre_order_yearly_summary
+
+        var description: String {
+            rawValue
+        }
+
+        typealias Filter = DownloadSalesAndTrendsReports.Filter
+
+        struct RequestParams {
+            let type: Filter.ReportType
+            let subType: Filter.ReportSubType
+            let frequency: Filter.Frequency
+            let version: Version
+        }
+
+        enum Version: String {
+            case _1_0 = "1_0"
+            case _1_2 = "1_2"
+        }
+
+        var params: RequestParams {
+            switch self {
+            case .sales_daily_summary:
+                return .init(type: .SALES, subType: .SUMMARY, frequency: .DAILY, version: ._1_0)
+            case .sales_weekly_summary:
+                return .init(type: .SALES, subType: .SUMMARY, frequency: .WEEKLY, version: ._1_0)
+            case .sales_monthly_summary:
+                return .init(type: .SALES, subType: .SUMMARY, frequency: .MONTHLY, version: ._1_0)
+            case .sales_yearly_summary:
+                return .init(type: .SALES, subType: .SUMMARY, frequency: .YEARLY, version: ._1_0)
+            case .sales_weekly_opt_in:
+                return .init(type: .SALES, subType: .OPT_IN, frequency: .WEEKLY, version: ._1_0)
+            case .subscription_daily_summary:
+                return .init(type: .SUBSCRIPTION, subType: .SUMMARY, frequency: .DAILY, version: ._1_2)
+            case .subscription_event_daily_summary:
+                return .init(type: .SUBSCRIPTION_EVENT, subType: .SUMMARY, frequency: .DAILY, version: ._1_2)
+            case .subscriber_daily_detailed:
+                return .init(type: .SUBSCRIBER, subType: .DETAILED, frequency: .DAILY, version: ._1_2)
+            case .newsstand_daily_detailed:
+                return .init(type: .NEWSSTAND, subType: .DETAILED, frequency: .DAILY, version: ._1_0)
+            case .newsstand_weekly_detailed:
+                return .init(type: .NEWSSTAND, subType: .DETAILED, frequency: .WEEKLY, version: ._1_0)
+            case .pre_order_daily_summary:
+                return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .DAILY, version: ._1_0)
+            case .pre_order_weekly_summary:
+                return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .WEEKLY, version: ._1_0)
+            case .pre_order_monthly_summary:
+                return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .MONTHLY, version: ._1_0)
+            case .pre_order_yearly_summary:
+                return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .YEARLY, version: ._1_0)
+            }
+        }
+    }
 
     static var configuration = CommandConfiguration(
         commandName: "sales",
@@ -15,8 +92,13 @@ struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
     @OptionGroup()
     var common: CommonOptions
 
-    @Argument(help: "Frequency of the report to download. (\(Filter.Frequency.allCases.description))")
-    var frequency: Filter.Frequency
+    @Argument(
+        help: ArgumentHelp(
+            "Sales and trends report filters.",
+            discussion: "Possibable values: \(ReportFilter.allCases)"
+        )
+    )
+    var filter: ReportFilter
 
     @Argument(help:
         ArgumentHelp(
@@ -26,31 +108,24 @@ struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
     )
     var reportDate: String
 
-    @Argument(help: "The report to download.  (\(Filter.ReportType.allCases))")
-    var reportType: Filter.ReportType
-
-    @Argument(help: "The report sub type to download.  (\(Filter.ReportSubType.allCases.description))")
-    var reportSubType: Filter.ReportSubType
-
     @Argument(help: "Your vendor number.")
     var vendorNumber: String
 
     @Argument(help: "The downloaded report file name.")
     var outputFilename: String
 
-    @Option(help: "The version of the report.")
-    var version: [String]
-
     func run() throws {
         let service = try makeService()
 
+        let requestFilters = filter.params
+
         let result = try service.downloadSales(
-            frequency: [frequency],
-            reportType: [reportType],
-            reportSubType: [reportSubType],
+            frequency: [requestFilters.frequency],
+            reportType: [requestFilters.type],
+            reportSubType: [requestFilters.subType],
             vendorNumber: [vendorNumber],
             reportDate: [reportDate],
-            version: version
+            version: [requestFilters.version.rawValue]
         )
 
         try ReportProcessor(path: outputFilename).write(result)
