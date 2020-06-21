9d45d149b39fcbed88c12e7d119425b95d8aeef1
Decheng <chinadecheng@gmail.com>
2020-06-22 09:47:16 +1000

    Download sales input, snake cased var name -> camel cased

--- Sources/AppStoreConnectCLI/Commands/Reports/DownloadSalesAndTrendsReportsCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Reports/DownloadSalesAndTrendsReportsCommand.swift
@@ -6,32 +6,31 @@ import FileSystem
 
 struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
 
-    // swiftlint:disable identifier_name
     enum ReportFilter: String, ExpressibleByArgument, CaseIterable, CustomStringConvertible {
 
         // SALES
-        case sales_daily_summary
-        case sales_weekly_summary
-        case sales_monthly_summary
-        case sales_yearly_summary
-        case sales_weekly_opt_in
+        case salesDailySummary = "sales_daily_summary"
+        case salesWeeklySummary = "sales_weekly_summary"
+        case salesMonthlySummary = "sales_monthly_summary"
+        case salesYearlySummary = "sales_yearly_summary"
+        case salesWeeklyOptIn = "sales_weekly_opt_in"
 
         // SUBSCRIPTION
-        case subscription_daily_summary
-        case subscription_event_daily_summary
+        case subscriptionDailySummary = "subscription_daily_summary"
+        case subscriptionEventDailySummary = "subscription_event_daily_summary"
 
         // SUBSCRIBER
-        case subscriber_daily_detailed
+        case subscriberDailyDetailed = "subscriber_daily_detailed"
 
         // NEWSSTAND
-        case newsstand_daily_detailed
-        case newsstand_weekly_detailed
+        case newsstandDailyDetailed = "newsstand_daily_detailed"
+        case newsstandWeeklyDetailed = "newsstand_weekly_detailed"
 
         // PRE_ORDER
-        case pre_order_daily_summary
-        case pre_order_weekly_summary
-        case pre_order_monthly_summary
-        case pre_order_yearly_summary
+        case preOrderDailySummary = "pre_order_daily_summary"
+        case preOrderWeeklySummary = "pre_order_weekly_summary"
+        case preOrderMonthlySummary = "pre_order_monthly_summary"
+        case preOrderYearlySummary = "pre_order_yearly_summary"
 
         var description: String {
             rawValue
@@ -46,6 +45,7 @@ struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
             let version: Version
         }
 
+        // swiftlint:disable identifier_name
         enum Version: String {
             case _1_0 = "1_0"
             case _1_2 = "1_2"
@@ -53,33 +53,33 @@ struct DownloadSalesAndTrendsReportsCommand: CommonParsableCommand {
 
         var params: RequestParams {
             switch self {
-            case .sales_daily_summary:
+            case .salesDailySummary:
                 return .init(type: .SALES, subType: .SUMMARY, frequency: .DAILY, version: ._1_0)
-            case .sales_weekly_summary:
+            case .salesWeeklySummary:
                 return .init(type: .SALES, subType: .SUMMARY, frequency: .WEEKLY, version: ._1_0)
-            case .sales_monthly_summary:
+            case .salesMonthlySummary:
                 return .init(type: .SALES, subType: .SUMMARY, frequency: .MONTHLY, version: ._1_0)
-            case .sales_yearly_summary:
+            case .salesYearlySummary:
                 return .init(type: .SALES, subType: .SUMMARY, frequency: .YEARLY, version: ._1_0)
-            case .sales_weekly_opt_in:
+            case .salesWeeklyOptIn:
                 return .init(type: .SALES, subType: .OPT_IN, frequency: .WEEKLY, version: ._1_0)
-            case .subscription_daily_summary:
+            case .subscriptionDailySummary:
                 return .init(type: .SUBSCRIPTION, subType: .SUMMARY, frequency: .DAILY, version: ._1_2)
-            case .subscription_event_daily_summary:
+            case .subscriptionEventDailySummary:
                 return .init(type: .SUBSCRIPTION_EVENT, subType: .SUMMARY, frequency: .DAILY, version: ._1_2)
-            case .subscriber_daily_detailed:
+            case .subscriberDailyDetailed:
                 return .init(type: .SUBSCRIBER, subType: .DETAILED, frequency: .DAILY, version: ._1_2)
-            case .newsstand_daily_detailed:
+            case .newsstandDailyDetailed:
                 return .init(type: .NEWSSTAND, subType: .DETAILED, frequency: .DAILY, version: ._1_0)
-            case .newsstand_weekly_detailed:
+            case .newsstandWeeklyDetailed:
                 return .init(type: .NEWSSTAND, subType: .DETAILED, frequency: .WEEKLY, version: ._1_0)
-            case .pre_order_daily_summary:
+            case .preOrderDailySummary:
                 return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .DAILY, version: ._1_0)
-            case .pre_order_weekly_summary:
+            case .preOrderWeeklySummary:
                 return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .WEEKLY, version: ._1_0)
-            case .pre_order_monthly_summary:
+            case .preOrderMonthlySummary:
                 return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .MONTHLY, version: ._1_0)
-            case .pre_order_yearly_summary:
+            case .preOrderYearlySummary:
                 return .init(type: .PRE_ORDER, subType: .SUMMARY, frequency: .YEARLY, version: ._1_0)
             }
         }
