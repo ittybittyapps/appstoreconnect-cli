927392a64f80e1fba39085e4db1e7e432831ec47
Decheng <chinadecheng@gmail.com>
2020-07-22 10:04:40 +1000

    Implement create profile command

--- Sources/AppStoreConnectCLI/Commands/Profiles/CreateProfileCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/Profiles/CreateProfileCommand.swift
@@ -5,6 +5,7 @@ import ArgumentParser
 import Foundation
 
 struct CreateProfileCommand: CommonParsableCommand {
+
     static var configuration = CommandConfiguration(
         commandName: "create",
         abstract: "Create a new provisioning profile.")
@@ -13,21 +14,35 @@ struct CreateProfileCommand: CommonParsableCommand {
     var common: CommonOptions
 
     @Argument(help: "The name of the provisioning profile to create.")
-    var string: String
+    var name: String
 
-    @Argument(help: "The type of profile to create \(ProfileType.allCases)")
+    @Argument(help: "The type of profile to create \(ProfileType.allCases).")
     var profileType: ProfileType
 
     @Argument(help: "The reverse-DNS bundle ID identifier to associate with this profile (must already exist).")
     var bundleId: String
 
-    @Argument(help: "Certificates") // TODO
+    @Option(
+        parsing: .upToNextOption,
+        help: "The resource ids of Certificates."
+    )
     var certificates: [String]
 
-    @Option(help: "Devices") // TODO
+    @Option(help: "The resource ids of Devices.")
     var devices: [String]
 
     func run() throws {
-        fatalError("Not implementedf")
+        let service = try makeService()
+
+        let profile = try service.createProfile(
+            name: name,
+            bundleId: bundleId,
+            profileType: profileType,
+            certificateIds: certificates,
+            deviceIds: devices
+        )
+
+        [profile].render(format: common.outputFormat)
     }
+
 }
