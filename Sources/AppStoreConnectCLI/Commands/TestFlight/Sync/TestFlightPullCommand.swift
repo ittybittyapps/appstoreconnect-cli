bc390e0727f684c0e5d5fdd147fe8a992967152a
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-08-27 14:12:03 +1000

    Amend testflight sync / pull help text

--- Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightPullCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightPullCommand.swift
@@ -8,18 +8,20 @@ struct TestFlightPullCommand: CommonParsableCommand {
 
     static var configuration = CommandConfiguration(
         commandName: "pull",
-        abstract: "Pull down existing TestFlight configuration, refreshing local configuration files."
+        abstract: "Pull TestFlight configuration, overwriting local configuration files."
     )
 
     @OptionGroup()
     var common: CommonOptions
 
-    @Option(parsing: .upToNextOption, help: "Filter the app configurations by the specified bundle IDs")
-    var filterBundleIds: [String]
+    @Option(
+        parsing: .upToNextOption,
+        help: "Filter by only including apps with the specified bundleIds in the configuration"
+    ) var filterBundleIds: [String]
 
     @Option(
         default: "./config/apps",
-        help: "Path to the folder containing the TestFlight configuration."
+        help: "Path to output/write the TestFlight configuration."
     ) var outputPath: String
 
     func run() throws {
--- Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightSyncCommand.swift
+++ Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightSyncCommand.swift
@@ -5,7 +5,7 @@ import ArgumentParser
 struct TestFlightSyncCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "sync",
-        abstract: "Synchronize with Testflight via configuration files.",
+        abstract: "Synchronize with TestFlight using configuration files.",
         subcommands: [
             TestFlightPullCommand.self,
         ]
