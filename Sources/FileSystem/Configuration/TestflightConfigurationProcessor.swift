36d26979d80376939ff5122c831938961cf4c9c7
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-08-27 18:48:22 +1000

    Add a writeConfiguration stub func to TestflightConfigurationProcessor

--- Sources/FileSystem/Configuration/TestflightConfigurationProcessor.swift
+++ Sources/FileSystem/Configuration/TestflightConfigurationProcessor.swift
@@ -1,6 +1,7 @@
 // Copyright 2020 Itty Bitty Apps Pty Ltd
 
 import Foundation
+import Model
 
 public struct TestflightConfigurationProcessor: ResourceWriter {
 
@@ -8,6 +9,14 @@ public struct TestflightConfigurationProcessor: ResourceWriter {
 
     public init(path: ResourcePath) {
         self.path = path
+    }
+
+    public func writeConfiguration(
+        apps: [Model.App],
+        testers: [Model.BetaTester],
+        groups: [Model.BetaGroup]
+    ) {
+        // TODO: Convert models to a TestflightConfiguration and write it to disk
     }
 
 }
