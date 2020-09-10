77ed1b704215449bf5f4704ab7262d68ee99a880
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-09-10 17:20:25 +1000

    Add adding and removing beta testers to app changes

--- Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightProgramDifference.swift
+++ Sources/AppStoreConnectCLI/Commands/TestFlight/Sync/TestFlightProgramDifference.swift
@@ -8,15 +8,19 @@ struct TestFlightProgramDifference {
     enum Change {
         case addBetaGroup(BetaGroup)
         case removeBetaGroup(BetaGroup)
-        case addBetaTester(BetaTester, toBetaGroups: [BetaGroup])
-        case removeBetaTester(BetaTester, fromBetaGroups: [BetaGroup])
+        case addBetaTesterToApps(BetaTester, [App])
+        case removeBetaTesterFromApps(BetaTester, [App])
+        case addBetaTesterToGroups(BetaTester, [BetaGroup])
+        case removeBetaTesterFromGroups(BetaTester, [BetaGroup])
 
         var description: String {
 
             let operation: String = {
                 switch self {
-                case .addBetaGroup, .addBetaTester: return "added to"
-                case .removeBetaGroup, .removeBetaTester: return "removed from"
+                case .addBetaGroup, .addBetaTesterToGroups, .addBetaTesterToApps:
+                    return "added to"
+                case .removeBetaGroup, .removeBetaTesterFromGroups, .removeBetaTesterFromApps:
+                    return "removed from"
                 }
             }()
 
@@ -25,11 +29,18 @@ struct TestFlightProgramDifference {
                 let name = betaGroup.groupName ?? ""
                 let bundleId = betaGroup.app?.bundleId ?? ""
 
-                return "Beta group named: \(name) " +
-                    "will be \(operation) app with bundleId: \(bundleId)"
+                return "Beta Group named: \(name) will be \(operation) app: \(bundleId)"
 
-            case .addBetaTester(let betaTester, let betaGroups),
-                 .removeBetaTester(let betaTester, let betaGroups):
+            case .addBetaTesterToApps(let betaTester, let apps),
+                 .removeBetaTesterFromApps(let betaTester, let apps):
+                let email = betaTester.email ?? ""
+                let bundleIds = apps.compactMap(\.bundleId).joined(separator: ", ")
+
+                return "Beta Tester with email: \(email) " +
+                    "will be \(operation) apps: \(bundleIds)"
+
+            case .addBetaTesterToGroups(let betaTester, let betaGroups),
+                 .removeBetaTesterFromGroups(let betaTester, let betaGroups):
                 let email = betaTester.email ?? ""
                 let groupNames = betaGroups.compactMap(\.groupName).joined(separator: ", ")
                 let bundleIds = betaGroups.compactMap(\.app?.bundleId).joined(separator: ", ")
@@ -58,21 +69,26 @@ struct TestFlightProgramDifference {
 
         // Testers
         let newTesters = local.testers.filter { !remote.testers.map(\.email).contains($0.email) }
-        changes += newTesters.map { Change.addBetaTester($0, toBetaGroups: $0.betaGroups) }
+
+        changes += newTesters.map { betaTester -> Change in
+            betaTester.betaGroups.isEmpty
+                ? .addBetaTesterToApps(betaTester, betaTester.apps)
+                : .addBetaTesterToGroups(betaTester, betaTester.betaGroups)
+        }
 
         for remoteTester in remote.testers {
             if let localTester = local.testers.first(where: { $0.email == remoteTester.email }) {
                 let groupsToAdd = localTester.betaGroups
                     .filter { !remoteTester.betaGroups.map(\.id).contains($0.id) }
-                let addAction = Change.addBetaTester(localTester, toBetaGroups: groupsToAdd)
+                let addAction = Change.addBetaTesterToGroups(localTester, groupsToAdd)
                 changes += groupsToAdd.isNotEmpty ? [addAction] : []
 
                 let groupsToRemove = remoteTester.betaGroups
                     .filter { !localTester.betaGroups.map(\.id).contains($0.id) }
-                let removeAction = Change.removeBetaTester(localTester, fromBetaGroups: groupsToRemove)
+                let removeAction = Change.removeBetaTesterFromGroups(localTester, groupsToRemove)
                 changes += groupsToRemove.isNotEmpty ? [removeAction] : []
             } else if remoteTester.betaGroups.isNotEmpty {
-                changes.append(.removeBetaTester(remoteTester, fromBetaGroups: remoteTester.betaGroups))
+                changes.append(.removeBetaTesterFromGroups(remoteTester, remoteTester.betaGroups))
             }
         }
 
