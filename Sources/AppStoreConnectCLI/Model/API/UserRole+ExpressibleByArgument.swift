31b16cf5fd68f941ef3b97381adebc6278e4a639
Decheng <chinadecheng@gmail.com>
2020-11-26 14:13:22 +1100

    Separate UserInfoArguments from InviteUserCommand For reuse purpose

--- Sources/AppStoreConnectCLI/Model/API/UserRole+ExpressibleByArgument.swift
+++ Sources/AppStoreConnectCLI/Model/API/UserRole+ExpressibleByArgument.swift
@@ -4,8 +4,18 @@ import ArgumentParser
 import AppStoreConnect_Swift_SDK
 import Foundation
 
-extension UserRole: ExpressibleByArgument {
+extension UserRole: ExpressibleByArgument, CustomStringConvertible {
+    public typealias AllCases = [UserRole]
+
     public init?(argument: String) {
         self.init(rawValue: argument.uppercased())
+    }
+
+    public static var allCases: AllCases {
+        [.accessToReports, .accountHolder, .admin, .appManager, .customerSupport, .developer, .finance, .marketing, .readOnly, .sales, .technical]
+    }
+
+    public var description: String {
+        rawValue.lowercased()
     }
 }
